63a550b66527e973fa27e6e5cb634ba820abd2ce
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-02-26 15:32:09 -0400

    model test : CollectionEntry

--- packages/stream_feed/test/models_test.dart
+++ packages/stream_feed/test/models_test.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
+import 'dart:math';
 
 import 'package:stream_feed_dart/src/core/models/activity.dart';
+import 'package:stream_feed_dart/src/core/models/group.dart';
 import 'package:stream_feed_dart/stream_feed.dart';
 import 'package:test/test.dart';
 
@@ -28,7 +30,7 @@ void main() {
 // - [ ] Audio
 // - [ ] Image
 // - [ ] User
-  test('description', () {
+  test('Activity', () {
     // final result = Activity(
     //     actor: "test",
     //     verb: "test",
@@ -52,5 +54,33 @@ void main() {
     expect(DateTime.parse("2001-09-11T00:01:02.000"), activity.time);
     expect(<FeedId>[FeedId("slug", "id")], activity.to);
     // expect(result, activity);//TODO: there is something wrong with equatable or json_serializable
+  });
+
+  test("CollectionEntry", () {
+    final entry = CollectionEntry(
+        id: "test",
+        collection: "test",
+        foreignId: "test",
+        data: {"test": "test"},
+        createdAt: DateTime.parse("2001-09-11T00:01:02.000"),
+        updatedAt: DateTime.parse("2001-09-11T00:01:02.000"));
+    final entryJson = json.decode(
+        '{"id": "test", "collection": "test", "foreign_id": "test", "data": {"test": "test"}, "created_at": "2001-09-11T00:01:02.000", "updated_at": "2001-09-11T00:01:02.000"}');
+    final expectedEntryJson = {
+      "id": "test",
+      "collection": "test",
+      "foreign_id": "test",
+      "data": {"test": "test"},
+      "created_at": "2001-09-11T00:01:02.000",
+      "updated_at": "2001-09-11T00:01:02.000"
+    };
+    expect(entryJson, expectedEntryJson);
+    final entryFromJson = CollectionEntry.fromJson(expectedEntryJson);
+    expect("test", entryFromJson.collection);
+    expect(DateTime.parse("2001-09-11T00:01:02.000"), entryFromJson.createdAt);
+    expect(DateTime.parse("2001-09-11T00:01:02.000"), entryFromJson.updatedAt);
+    expect({'test': 'test'}, entryFromJson.data);
+    expect("test", entryFromJson.foreignId);
+    expect("test", entryFromJson.id);
   });
 }
