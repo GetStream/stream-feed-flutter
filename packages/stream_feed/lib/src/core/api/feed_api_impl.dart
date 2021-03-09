659d9eb7c9d3ece7d9fe2d48854d8d66c80a750b
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-03-09 07:40:46 -0400

    add trailing trailing comma for lint

--- packages/stream_feed/lib/src/core/api/feed_api_impl.dart
+++ packages/stream_feed/lib/src/core/api/feed_api_impl.dart
@@ -55,9 +55,9 @@ class FeedApiImpl implements FeedApi {
     checkArgument(activityCopyLimit >= 0,
         'Activity copy limit should be a non-negative number');
     checkArgument(
-        activityCopyLimit <= Default.maxActivityCopyLimit,
-        // ignore: lines_longer_than_80_chars
-        'Activity copy limit should be less then ${Default.maxActivityCopyLimit}');
+      activityCopyLimit <= Default.maxActivityCopyLimit,
+      'Activity copy limit should be less then ${Default.maxActivityCopyLimit}',
+    );
     return client.post(
       Routes.buildFeedUrl(sourceFeed, 'following'),
       headers: {'Authorization': '$token'},
