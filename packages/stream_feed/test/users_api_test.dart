01698214dea5f2eea7ebb0ce0588a60ed75b20ae
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-03-09 17:19:35 -0400

    Unit Test Users API Add

--- packages/stream_feed/test/users_api_test.dart
+++ packages/stream_feed/test/users_api_test.dart
@@ -6,6 +6,7 @@ import 'package:dio/dio.dart';
 import 'package:mockito/mockito.dart';
 import 'package:stream_feed_dart/src/core/http/http_client.dart';
 import 'package:stream_feed_dart/src/core/http/token.dart';
+import 'package:stream_feed_dart/src/core/models/user.dart';
 
 import 'utils.dart';
 
@@ -33,6 +34,35 @@ Future<void> main() async {
         Routes.buildUsersUrl('$id/'),
         headers: {'Authorization': '$token'},
         queryParameters: {'with_follow_counts': withFollowCounts},
+      )).called(1);
+    });
+    test('Add', () async {
+      const token = Token('dummyToken');
+
+      const id = 'john-doe';
+
+      const data = {
+        'name': 'John Doe',
+        'occupation': 'Software Engineer',
+        'gender': 'male',
+      };
+      final user = User(id: id, data: data);
+      const getOrCreate = false;
+      when(mockClient.post<Map>(
+        Routes.buildUsersUrl(),
+        headers: {'Authorization': '$token'},
+        queryParameters: {'get_or_create': getOrCreate},
+        data: user,
+      )).thenAnswer((_) async =>
+          Response(data: jsonFixture('user.json'), statusCode: 200));
+
+      await usersApi.add(token, id, data);
+
+      verify(mockClient.post<Map>(
+        Routes.buildUsersUrl(),
+        headers: {'Authorization': '$token'},
+        queryParameters: {'get_or_create': getOrCreate},
+        data: user,
       )).called(1);
     });
   });
