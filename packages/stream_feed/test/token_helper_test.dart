be8f21289a2594d8c0e79edb10d19cc8d9c78da0
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-03-10 15:07:42 -0400

    Test TokenHelper buildActivityToken

--- packages/stream_feed/test/token_helper_test.dart
+++ packages/stream_feed/test/token_helper_test.dart
@@ -63,6 +63,21 @@ main() {
       expect(payloadJson['resource'], 'reactions');
       expect(payloadJson['feed_id'], '*');
     });
+
+    test('buildActivityToken', () {
+      final expiresAt = DateTime(2021, 03, 08);
+      final feedToken =
+          TokenHelper.buildActivityToken("secret", TokenAction.any);
+      final tokenParts = feedToken.token.split('.');
+      final header = tokenParts[0];
+      final payload = tokenParts[1];
+      expect(header, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9');
+      final payloadStr = b64urlEncRfc7515Decode(payload);
+      final payloadJson = json.decode(payloadStr);
+      expect(payloadJson['action'], '*');
+      expect(payloadJson['resource'], 'activities');
+      expect(payloadJson['feed_id'], '*');
+    });
   });
 }
 
