68b4f4448c16aa773cade331df6b53e11a8fd89b
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-03-18 11:13:37 -0400

    remove_from_coverage package

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -6,5 +6,6 @@ set -e
 dart pub global activate coverage
 
 dart test --coverage="coverage"
-
+pub global activate remove_from_coverage
+pub global run remove_from_coverage:remove_from_coverage -f coverage/lcov.info -r '.g.dart$'
 format_coverage --lcov --in=coverage --out=coverage.lcov --packages=.packages --report-on=lib
