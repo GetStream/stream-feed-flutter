1d05894d8fd243e2ede3947d269329da7c5f6837
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-03-18 11:39:54 -0400

    coverage.sh should be fixed now

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -5,7 +5,6 @@ set -e
 
 dart pub global activate coverage
 
-dart test --coverage="coverage"
+flutter test --coverage
 pub global activate remove_from_coverage
-pub global run remove_from_coverage:remove_from_coverage -f packages/stream_feed/coverage/lcov.info -r '.g.dart$'
-format_coverage --lcov --in=coverage --out=coverage.lcov --packages=.packages --report-on=lib
+pub global run remove_from_coverage:remove_from_coverage -f coverage/lcov.info -r '.g.dart$'
