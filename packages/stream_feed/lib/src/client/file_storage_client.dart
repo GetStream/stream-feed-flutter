34f03b502e9d24e7e26b4aef9aedfac533661324
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-03-23 14:21:00 -0400

    document delete in FileStorageClient

--- packages/stream_feed/lib/src/client/file_storage_client.dart
+++ packages/stream_feed/lib/src/client/file_storage_client.dart
@@ -26,6 +26,15 @@ class FileStorageClient {
     return files.upload(token, file);
   }
 
+  /// Delete a file using the url returned by the APIs
+  /// /// Usage:
+  /// ```dart
+  ///   await files.delete('fileUrl');
+  /// ```
+  ///
+  /// Parameters:
+  /// - [url] : the url of the file you want to delete
+  /// API docs: [delete](https://getstream.io/activity-feeds/docs/flutter-dart/files_introduction/?language=dart#delete)
   Future<void> delete(String url) {
     final token =
         userToken ?? TokenHelper.buildFilesToken(secret, TokenAction.delete);
