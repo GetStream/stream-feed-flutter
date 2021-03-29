874eae112ecf3a49320a155be4141e7c1b7ab4da
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-03-29 09:02:35 -0400

    event emitter test wip

---
+++ packages/faye_dart/test/event_emitter_test.dart
@@ -0,0 +1,21 @@
+import 'package:faye_dart/src/event_emitter.dart';
+import 'package:test/test.dart';
+
+main() {
+  test('EventEmitter', () {
+    final eventEmitter = EventEmitter();
+    final logs = [];
+    eventEmitter.addListener('event', (data) {
+      addToLogs(logs, data);
+    });
+    expect(eventEmitter.mounted, true);
+    expect(eventEmitter.hasListeners('event'), true);
+    eventEmitter.emit('event', 'data');
+    expect(logs, ['data']);
+
+  });
+}
+
+void addToLogs(List<dynamic> logs, data) {
+  logs.add(data);
+}
