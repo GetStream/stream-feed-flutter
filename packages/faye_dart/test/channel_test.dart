f7f53c12c20f29bec3f63d69d519beeb01a9c6db
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-03-29 10:07:04 -0400

    test channel: bind, unbind, trigger

--- packages/faye_dart/test/channel_test.dart
+++ packages/faye_dart/test/channel_test.dart
@@ -3,31 +3,61 @@ import 'package:faye_dart/src/message.dart';
 import 'package:test/test.dart';
 
 main() {
-  test('Channel', () {
-    var _ext = {
-      "api_key": 'Client.shared.apiKey',
-      "signature": 'Client.shared.token',
-      "user_id": 'notificationChannelName',
-    };
-    var _name = "name";
-    final channel = Channel(
-      name: _name,
-    )..ext = _ext;
+  group('Channel', () {
+    test('event listener', () {
+      var _ext = {
+        "api_key": 'Client.shared.apiKey',
+        "signature": 'Client.shared.token',
+        "user_id": 'notificationChannelName',
+      };
+      var _name = "name";
+      final channel = Channel(
+        name: _name,
+      )..ext = _ext;
 
-    final logs = [];
-    var listener = (data) {
-      logs.add(data);
-    };
-    channel.addListener('event', listener);
-    expect(channel.mounted, true);
-    var event = 'event';
-    expect(channel.hasListeners(event), true);
-    var message = Message("bayeuxChannel");
-    channel.emit('event', message);
-    expect(logs, [message]);
-    expect(channel.ext, _ext);
-    expect(channel.name, "/$_name");
-    channel.removeListener('event', listener);
-    expect(channel.hasListeners(event), false);
+      final logs = [];
+      var listener = (data) {
+        logs.add(data);
+      };
+      channel.addListener('event', listener);
+      expect(channel.mounted, true);
+      var event = 'event';
+      expect(channel.hasListeners(event), true);
+      var message = Message("bayeuxChannel");
+      channel.emit('event', message);
+      expect(logs, [message]);
+      expect(channel.ext, _ext);
+      expect(channel.name, "/$_name");
+      channel.removeListener('event', listener);
+      expect(channel.hasListeners(event), false);
+    });
+
+    test('subscription', () {
+      var _ext = {
+        "api_key": 'Client.shared.apiKey',
+        "signature": 'Client.shared.token',
+        "user_id": 'notificationChannelName',
+      };
+      var _name = "name";
+      final channel = Channel(
+        name: _name,
+      )..ext = _ext;
+
+      final logs = [];
+      var listener = (data) {
+        logs.add(data);
+      };
+      channel.bind('event', listener);
+      expect(channel.mounted, true);
+      var event = 'event';
+      expect(channel.hasListeners(event), true);
+      var message = Message("bayeuxChannel");
+      channel.trigger('event', message);
+      expect(logs, [message]);
+      expect(channel.ext, _ext);
+      expect(channel.name, "/$_name");
+      channel.unbind('event', listener);
+      expect(channel.hasListeners(event), false);
+    });
   });
 }
