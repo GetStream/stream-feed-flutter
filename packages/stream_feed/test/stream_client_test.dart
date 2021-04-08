40cbc08ed86037c5e62046f77bb49e59c029281c
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-04-08 16:38:40 -0400

    test enrichUrl

--- packages/stream_feed/test/stream_client_test.dart
+++ packages/stream_feed/test/stream_client_test.dart
@@ -2,12 +2,19 @@ import 'package:stream_feed_dart/src/core/http/stream_http_client.dart';
 import 'package:test/test.dart';
 
 main() {
-  test('StreamHttpClient', () {
+  group('StreamHttpClient', () {
     final streamHttpClient = StreamHttpClient("apiKey");
-    expect(streamHttpClient.httpClient.options.headers, {
-      'stream-auth-type': 'jwt',
-      'x-stream-client': isA<String>(), //'stream-feed-dart-client-macos-0.0.1',
-      'content-type': 'application/json; charset=utf-8'
+    test('headers', () {
+      expect(streamHttpClient.httpClient.options.headers, {
+        'stream-auth-type': 'jwt',
+        'x-stream-client':
+            isA<String>(), //'stream-feed-dart-client-macos-0.0.1',
+        'content-type': 'application/json; charset=utf-8'
+      });
+    });
+    test('enrichUrl', () {
+      expect(streamHttpClient.enrichUrl('feed', 'api'),
+          'https://api.stream-io-api.com/api/v1.0/feed');
     });
   });
 }
