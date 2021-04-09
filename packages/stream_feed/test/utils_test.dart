6320ee97f3f85c8e7b1ecba55ccf7e23063ae743
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-04-09 16:02:42 -0400

    test checkNotNull

--- packages/stream_feed/test/utils_test.dart
+++ packages/stream_feed/test/utils_test.dart
@@ -1,18 +1,27 @@
+import 'package:stream_feed_dart/src/core/util/extension.dart';
 import 'package:stream_feed_dart/src/core/util/serializer.dart';
 import 'package:test/test.dart';
 
 main() {
-  group('Serializer', () {
-    test('moveKeysToMapInPlace', () {
-    final serializer =
-        Serializer.moveKeysToMapInPlace({'test': 'test'}, ['test']);
-    expect(serializer, {'test': 'test'});
+  group('extension', () {
+    test('checkNotNull', () {
+      expect(
+          () => checkNotNull(null, 'your custom message'),
+          throwsA(predicate<ArgumentError>(
+              (e) => e.message == 'your custom message')));
+    });
   });
 
+  group('Serializer', () {
     test('moveKeysToMapInPlace', () {
-    final serializer =
-        Serializer.moveKeysToRoot({'test': 'test'}, ['test']);
-    expect(serializer, {'test': 'test', 'extra_data': {}});
-  });
+      final serializer =
+          Serializer.moveKeysToMapInPlace({'test': 'test'}, ['test']);
+      expect(serializer, {'test': 'test'});
+    });
+
+    test('moveKeysToMapInPlace', () {
+      final serializer = Serializer.moveKeysToRoot({'test': 'test'}, ['test']);
+      expect(serializer, {'test': 'test', 'extra_data': {}});
+    });
   });
 }
