6da9a096d677ff242b95fdc2e61ffbd08edf430d
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-04-14 07:08:00 -0400

    test NotificationFeed Client getEnrichedActivities

--- packages/stream_feed/lib/src/client/notification_feed.dart
+++ packages/stream_feed/lib/src/client/notification_feed.dart
@@ -30,7 +30,7 @@ class NotificationFeed extends AggregatedFeed {
       ...filter?.params ?? Default.filter.params,
       ...marker?.params ?? Default.marker.params,
     };
-    final token = TokenHelper.buildFeedToken(secret!, TokenAction.read, feedId);
+    final token =userToken?? TokenHelper.buildFeedToken(secret!, TokenAction.read, feedId);
     final result = await feed.getActivities(token, feedId, options);
     final data = (result.data!['results'] as List)
         .map((e) => NotificationGroup.fromJson(
@@ -54,7 +54,7 @@ class NotificationFeed extends AggregatedFeed {
       ...marker?.params ?? Default.marker.params,
       ...flags?.params ?? Default.enrichmentFlags.params,
     };
-    final token = TokenHelper.buildFeedToken(secret!, TokenAction.read, feedId);
+    final token =userToken ?? TokenHelper.buildFeedToken(secret!, TokenAction.read, feedId);
     final result = await feed.getEnrichedActivities(token, feedId, options);
     final data = (result.data['results'] as List)
         .map((e) => NotificationGroup.fromJson(e,
---
+++ packages/stream_feed/test/notification_client_test.dart
@@ -0,0 +1,104 @@
+import 'package:dio/dio.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:stream_feed_dart/src/client/aggregated_feed.dart';
+import 'package:stream_feed_dart/src/client/notification_feed.dart';
+import 'package:stream_feed_dart/src/core/http/token.dart';
+import 'package:stream_feed_dart/src/core/models/activity.dart';
+import 'package:stream_feed_dart/src/core/models/activity_marker.dart';
+import 'package:stream_feed_dart/src/core/models/enriched_activity.dart';
+import 'package:stream_feed_dart/src/core/models/enrichment_flags.dart';
+import 'package:stream_feed_dart/src/core/models/feed_id.dart';
+import 'package:stream_feed_dart/src/core/models/filter.dart';
+import 'package:stream_feed_dart/src/core/models/group.dart';
+import 'package:test/test.dart';
+
+import 'mock.dart';
+import 'utils.dart';
+
+main() {
+  group('NotificationFeed Client', () {
+    final api = MockFeedApi();
+    final feedId = FeedId('slug', 'userId');
+    const token = Token('dummyToken');
+    final client = NotificationFeed(feedId, api, userToken: token);
+    final dummyResponse = Response(
+        data: {},
+        requestOptions: RequestOptions(
+          path: '',
+        ),
+        statusCode: 200);
+    test('getActivities', () async {
+      const limit = 5;
+      const offset = 0;
+      final marker = ActivityMarker().allSeen();
+      final filter =
+          Filter().idGreaterThan('e561de8f-00f1-11e4-b400-0cc47a024be0');
+      final options = {
+        'limit': limit,
+        'offset': offset,
+        ...filter.params,
+        ...marker.params,
+      };
+      final rawActivities = [jsonFixture('group.json')];
+      when(() => api.getActivities(token, feedId, options))
+          .thenAnswer((_) async => Response(
+              data: {'results': rawActivities},
+              requestOptions: RequestOptions(
+                path: '',
+              ),
+              statusCode: 200));
+      final activities = await client.getActivities(
+          limit: limit, offset: offset, filter: filter, marker: marker);
+
+      expect(
+          activities,
+          rawActivities
+              .map((e) => NotificationGroup.fromJson(e!,
+                  (json) => Activity.fromJson(json as Map<String, dynamic>?)))
+              .toList(growable: false));
+      verify(() => api.getActivities(token, feedId, options)).called(1);
+    });
+
+    test('getEnrichedActivities', () async {
+      const limit = 5;
+      const offset = 0;
+      final marker = ActivityMarker().allSeen();
+      final filter =
+          Filter().idGreaterThan('e561de8f-00f1-11e4-b400-0cc47a024be0');
+      final flags =
+          EnrichmentFlags().withRecentReactions().withReactionCounts();
+      final options = {
+        'limit': limit,
+        'offset': offset,
+        ...filter.params,
+        ...marker.params,
+        ...flags.params
+      };
+
+      final rawActivities = [jsonFixture('group_enriched_activity.json')];
+      when(() => api.getEnrichedActivities(token, feedId, options))
+          .thenAnswer((_) async => Response(
+              data: {'results': rawActivities},
+              requestOptions: RequestOptions(
+                path: '',
+              ),
+              statusCode: 200));
+   final activities = await client.getEnrichedActivities(
+          limit: limit,
+          offset: offset,
+          filter: filter,
+          marker: marker,
+          flags: flags);
+
+      expect(
+          activities,
+          rawActivities
+              .map((e) => NotificationGroup.fromJson(
+                  e!,
+                  (json) =>
+                      EnrichedActivity.fromJson(json as Map<String, dynamic>?)))
+              .toList(growable: false));
+      verify(() => api.getEnrichedActivities(token, feedId, options)).called(1);
+    });
+  });
+}
