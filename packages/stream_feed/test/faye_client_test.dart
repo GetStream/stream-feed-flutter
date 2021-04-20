57e4446da3eae479e6dab5bd702a582fa1ad8000
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-04-20 16:13:40 -0400

    add CONTRIBUTING

--- packages/stream_feed/test/faye_client_test.dart
+++ packages/stream_feed/test/faye_client_test.dart
@@ -35,7 +35,7 @@ void main() async {
     await Future.delayed(const Duration(seconds: 3));
 
     expect(realTimeMessage, isNotNull);
-    expect(realTimeMessage!.newActivities.first.id, activity.id);
+    expect(realTimeMessage!.newActivities.first.id, activity.id);//TODO: this test is flaky
 
     addTearDown(subscription.cancel);
   });
