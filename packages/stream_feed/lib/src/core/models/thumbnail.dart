96e226a90367a309d71fade1c5631344788181fd
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-04-23 08:07:51 -0400

    make resizeType and cropTypes final in Thumbnail

--- packages/stream_feed/lib/src/core/models/thumbnail.dart
+++ packages/stream_feed/lib/src/core/models/thumbnail.dart
@@ -1,18 +1,19 @@
 import 'package:stream_feed/src/core/models/crop.dart';
 import 'package:stream_feed/src/core/models/resize.dart';
 
-/// Resize an image based on supplied [_width],[_height] and eventually [_type]
+/// Resize an image based on supplied [_width],[_height] and eventually [cropTypes],[resizeType]
 class Thumbnail {
   /// [Thumbnail] constructor
   Thumbnail(this._width, this._height,
-      {required this.resizeType, required this.cropTypes})
+      {this.resizeType = ResizeType.clip,
+      this.cropTypes = const [CropType.center]})
       : assert(_width > 0, 'Width should be a positive number'),
         assert(_height > 0, 'Height should be a positive number');
 
   final int _width;
   final int _height;
-  ResizeType resizeType = ResizeType.clip;
-  List<CropType> cropTypes = const [CropType.center];
+  final ResizeType resizeType;
+  final List<CropType> cropTypes;
 
   /// Serialize params
   Map<String, Object?> get params => <String, Object?>{
