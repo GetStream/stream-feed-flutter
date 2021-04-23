513b1beb1a6fba37eb871e7fe8210e7abfd876ee
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-04-23 17:18:11 -0400

    got ignored oups

---
+++ packages/stream_feed/lib/src/core/models/personalized_feed.dart
@@ -0,0 +1,35 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:stream_feed/src/core/models/activity.dart';
+import 'package:stream_feed/src/core/models/paginated.dart';
+
+part 'personalized_feed.g.dart';
+
+/// Paginated [Activity]
+@JsonSerializable(createToJson: true)
+class PersonalizedFeed extends Paginated<Activity> {
+  /// [PaginatedReactions] constructor
+  const PersonalizedFeed({
+    required this.version,
+    required this.offset,
+    required this.limit,
+    String? next,
+    List<Activity>?
+        results, //TODO: hmm I think it should be na enriched activity
+    String? duration,
+  }) : super(next, results, duration);
+
+  /// Deserilize json to [PaginatedReactions]
+  factory PersonalizedFeed.fromJson(Map<String, dynamic> json) =>
+      _$PersonalizedFeedFromJson(json);
+
+  @override
+  List<Object?> get props => [...super.props, version, offset, limit];
+
+  final String version;
+
+  final int offset;
+  final int limit;
+
+  /// Serialize to json
+  Map<String, dynamic> toJson() => _$PersonalizedFeedToJson(this);
+}
