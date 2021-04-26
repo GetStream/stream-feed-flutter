bb9b144da0d7acf6acdabf7c9f66f3e6862ac261
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-04-26 09:17:02 -0400

    get activity detail tests

--- packages/stream_feed/test/flat_client_test.dart
+++ packages/stream_feed/test/flat_client_test.dart
@@ -7,6 +7,7 @@ import 'package:stream_feed/src/core/models/enriched_activity.dart';
 import 'package:stream_feed/src/core/models/enrichment_flags.dart';
 import 'package:stream_feed/src/core/models/feed_id.dart';
 import 'package:stream_feed/src/core/models/filter.dart';
+import 'package:stream_feed/src/core/models/group.dart';
 import 'package:stream_feed/src/core/util/default.dart';
 import 'package:test/test.dart';
 
@@ -19,6 +20,37 @@ void main() {
     final feedId = FeedId('slug', 'userId');
     const token = Token('dummyToken');
     final client = FlatFeed(feedId, api, userToken: token);
+
+    test('getActivityDetail', () async {
+      const limit = 1;
+      const activityId = 'e561de8f-00f1-11e4-b400-0cc47a024be0';
+      final filter = Filter()
+          .idLessThanOrEqual(activityId)
+          .idGreaterThanOrEqual(activityId);
+      final options = {
+        'limit': limit,
+        'offset': Default.offset,
+        ...filter.params,
+        ...Default.marker.params,
+      };
+      final rawActivities = [jsonFixture('group.json')];
+      when(() => api.getActivities(token, feedId, options))
+          .thenAnswer((_) async => Response(
+              data: {'results': rawActivities},
+              requestOptions: RequestOptions(
+                path: '',
+              ),
+              statusCode: 200));
+      final activities = await client.getActivityDetail(activityId);
+
+      expect(
+          activities,
+          rawActivities
+              .map((e) => Activity.fromJson(e))
+              .toList(growable: false)
+              .first);
+      verify(() => api.getActivities(token, feedId, options)).called(1);
+    });
 
     test('getActivities', () async {
       const limit = 5;
