503c6b5e8221ab5c40ea81e2ec199f49e61c2f9b
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-04-28 07:46:37 -0400

    advice toJson

--- packages/faye_dart/test/models_test.dart
+++ packages/faye_dart/test/models_test.dart
@@ -2,13 +2,18 @@ import 'package:faye_dart/src/message.dart';
 import 'package:test/test.dart';
 
 main() {
-  test('Advice', () {
-    final advice = Advice.fromJson({
-      'interval': 2,
-      'reconnect': '',
-      'timeout': 2
-    }); //Advice(interval: 2, reconnect: '', timeout: 2);
-    expect(advice, Advice(interval: 2, reconnect: '', timeout: 2));
+  group('Advice', () {
+    final json = {'interval': 2, 'reconnect': '', 'timeout': 2};
+    final advice = Advice(interval: 2, reconnect: '', timeout: 2);
+    test('fromJson', () {
+      final adviceFromJson = Advice.fromJson(
+          json); //Advice(interval: 2, reconnect: '', timeout: 2);
+      expect(adviceFromJson, advice);
+    });
+
+    test('toJson', () {
+      expect(advice.toJson(), json);
+    });
   });
 
   group('Message', () {
