9d393979730eb5d8f113ebd3b74e536b50943023
Sahil Kumar <xdsahil@gmail.com>
2021-05-07 22:21:48 +0530

    add generated files to repo
    Signed-off-by: Sahil Kumar <xdsahil@gmail.com>

---
+++ packages/stream_feed/lib/src/core/models/attachment_file.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'attachment_file.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+AttachmentFile _$AttachmentFileFromJson(Map json) {
+  return AttachmentFile(
+    path: json['path'] as String?,
+    name: json['name'] as String?,
+    bytes: _fromString(json['bytes'] as String?),
+    size: json['size'] as int?,
+  );
+}
+
+Map<String, dynamic> _$AttachmentFileToJson(AttachmentFile instance) =>
+    <String, dynamic>{
+      'path': instance.path,
+      'name': instance.name,
+      'bytes': _toString(instance.bytes),
+      'size': instance.size,
+    };
---
+++ packages/stream_feed/lib/src/core/models/personalized_feed.g.dart
@@ -0,0 +1,32 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'personalized_feed.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+PersonalizedFeed _$PersonalizedFeedFromJson(Map json) {
+  return PersonalizedFeed(
+    version: json['version'] as String,
+    offset: json['offset'] as int,
+    limit: json['limit'] as int,
+    next: json['next'] as String?,
+    results: (json['results'] as List<dynamic>?)
+        ?.map((e) => EnrichedActivity.fromJson((e as Map?)?.map(
+              (k, e) => MapEntry(k as String, e),
+            )))
+        .toList(),
+    duration: json['duration'] as String?,
+  );
+}
+
+Map<String, dynamic> _$PersonalizedFeedToJson(PersonalizedFeed instance) =>
+    <String, dynamic>{
+      'next': instance.next,
+      'results': instance.results?.map((e) => e.toJson()).toList(),
+      'duration': instance.duration,
+      'version': instance.version,
+      'offset': instance.offset,
+      'limit': instance.limit,
+    };
