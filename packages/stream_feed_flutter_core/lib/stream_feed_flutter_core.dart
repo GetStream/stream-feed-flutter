98f702f8cfb40288acf13c0a1e027266dec71f1b
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-05-17 08:16:40 -0400

    scaffold core

---
+++ packages/stream_feed_flutter_core/lib/stream_feed_flutter_core.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library stream_feed_flutter_core;
+
+export 'src/stream_feed_flutter_core_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
