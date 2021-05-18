d012afe69d7987743edba54767fc92325d74b1f1
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-05-18 09:07:12 -0400

    format

--- packages/stream_feed_flutter/test/stream_feed_flutter_test.dart
+++ packages/stream_feed_flutter/test/stream_feed_flutter_test.dart
@@ -13,7 +13,6 @@ void main() {
       await screenMatchesGolden(tester, 'love_active_icon');
     });
 
-
     testGoldens('StreamSvgIcon loveInactive', (tester) async {
       await tester.pumpWidgetBuilder(
         Center(child: StreamSvgIcon.loveInactive()),
