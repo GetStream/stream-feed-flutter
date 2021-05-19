0e73819241f46488d0620925c8e9090514a9438e
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-05-19 09:21:38 -0400

    update config

--- packages/stream_feed_flutter/test/flutter_test_config.dart
+++ packages/stream_feed_flutter/test/flutter_test_config.dart
@@ -1,9 +1,19 @@
 import 'dart:async';
+import 'dart:io';
 
-import 'package:flutter_test/flutter_test.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
 
 Future<void> testExecutable(FutureOr<void> Function() testMain) async {
-  await loadAppFonts();
-  return testMain();
+  return GoldenToolkit.runWithConfiguration(
+    () async {
+      await loadAppFonts();
+      await testMain();
+    },
+    config: GoldenToolkitConfiguration(
+      // Currently, goldens are not generated/validated in CI for this repo. We have settled on the goldens for this package
+      // being captured/validated by developers running on MacOSX. We may revisit this in the future if there is a reason to invest
+      // in more sophistication
+      skipGoldenAssertion: () => !Platform.isMacOS,
+    ),
+  );
 }
