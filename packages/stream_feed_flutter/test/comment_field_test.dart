70ae2c8b715ac6af7efd4de3a335157542e98b52
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-05-24 09:48:52 -0400

    update comment field test

--- packages/stream_feed_flutter/test/comment_field_test.dart
+++ packages/stream_feed_flutter/test/comment_field_test.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:golden_toolkit/golden_toolkit.dart';
 import 'package:stream_feed_flutter/src/comment_field.dart';
+import 'package:stream_feed_flutter/src/textarea.dart';
+import 'package:stream_feed_flutter/stream_feed_flutter.dart';
 
 main() {
-  testGoldens('CommentField', (tester) async {
-    await tester.pumpWidgetBuilder(
-      Container(width: 220, child: CommentField()),
-      // surfaceSize: const Size(200, 200),
-    );
-    await screenMatchesGolden(tester, 'comment_field');
+  testWidgets('CommentField', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(
+        home: Scaffold(
+      body: CommentField(),
+    )));
+    final avatar = find.byType(Avatar);
+    final textArea = find.byType(TextArea);
+    final button = find.byType(Button);
+    expect(avatar, findsOneWidget);
+    expect(textArea, findsOneWidget);
+    expect(button, findsOneWidget);
   });
 }
