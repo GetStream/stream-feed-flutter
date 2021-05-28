6a07f0070c374f4f39de3f244b30b0eefee33f09
Sahil Kumar <xdsahil@gmail.com>
2021-05-28 16:35:59 +0530

    (stream_feed): merge fixes
    Signed-off-by: Sahil Kumar <xdsahil@gmail.com>

--- example/lib/extension.dart
+++ example/lib/extension.dart
@@ -4,7 +4,7 @@ import 'package:stream_feed/stream_feed.dart';
 import 'client_provider.dart';
 
 extension ProviderX on BuildContext {
-  StreamClient get client => ClientProvider.of(this).client;
+  StreamFeedClient get client => ClientProvider.of(this).client;
 }
 
 extension Snackbar on BuildContext {
