61911f1108f471fa36b87edfc0eaa2c1a8cf13b0
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-06-04 11:49:32 -0400

    RepostButton: constructors sorted first + docs

--- packages/stream_feed_flutter/lib/src/repost_button.dart
+++ packages/stream_feed_flutter/lib/src/repost_button.dart
@@ -2,10 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:stream_feed_flutter/stream_feed_flutter.dart';
 
 class RepostButton extends StatelessWidget {
+  RepostButton({this.reaction, this.activity});
+
+  ///The reaction received from stream that should be repost when pressing the RepostButton.
   final Reaction? reaction;
+
+  ///The activity received for stream for which to show the repost button. This is used to initialize the toggle state and the counter.
   final EnrichedActivity? activity;
 
-  RepostButton({this.reaction, this.activity});
   @override
   Widget build(BuildContext context) {
     return ReactionToggleIcon(
