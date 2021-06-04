d2f718b7e645a5a674ed4ad9ca3a62fa1bab1198
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-06-04 11:29:35 -0400

    Like: onstructors sorted first + docs

--- packages/stream_feed_flutter/lib/src/like_button.dart
+++ packages/stream_feed_flutter/lib/src/like_button.dart
@@ -2,10 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:stream_feed_flutter/stream_feed_flutter.dart';
 
 class LikeButton extends StatelessWidget {
+  LikeButton({this.reaction, this.activity});
+
+  ///The reaction received from stream that should be liked when pressing the LikeButton.
   final Reaction? reaction;
+
+  /// The activity received from stream that should be liked when pressing the LikeButton.
   final EnrichedActivity? activity;
 
-  LikeButton({this.reaction, this.activity});
   @override
   Widget build(BuildContext context) {
     return ReactionToggleIcon(
