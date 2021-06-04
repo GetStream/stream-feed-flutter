c283d609ff3f4176ec4066b12830a4c7c7cba1db
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-06-04 15:42:49 -0400

    ReactionIcon : prepare callback with widget test

--- packages/stream_feed_flutter/test/reaction_icon_test.dart
+++ packages/stream_feed_flutter/test/reaction_icon_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
@@ -6,7 +7,7 @@ import 'package:stream_feed_flutter/stream_feed_flutter.dart';
 
 void main() {
   group('ReactionIcon', () {
-    testGoldens('repost', (tester) async {
+    testGoldens('repost golden', (tester) async {
       await tester.pumpWidgetBuilder(
         Center(
           child: ReactionIcon(
@@ -17,6 +18,22 @@ void main() {
         surfaceSize: const Size(100, 75),
       );
       await screenMatchesGolden(tester, 'repost');
+    });
+
+    testWidgets("repost on tap", (tester) async {
+      await tester.pumpWidget(MaterialApp(
+          home: Scaffold(
+        body: ReactionIcon(
+          icon: StreamSvgIcon.repost(),
+          count: 23,
+        ),
+      )));
+
+      final icon = find.byType(StreamSvgIcon);
+      final activeIcon = tester.widget<StreamSvgIcon>(icon);
+      final count = find.text('23');
+      expect(count, findsOneWidget);
+      expect(activeIcon.assetName, StreamSvgIcon.repost().assetName);
     });
   });
 }
