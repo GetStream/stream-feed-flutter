5bbae99dc17e7c44e57aa2d760a70fd07bb19c03
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-06-07 09:32:42 -0400

    add jsonKey

--- packages/stream_feed_flutter/lib/src/avatar.dart
+++ packages/stream_feed_flutter/lib/src/avatar.dart
@@ -4,14 +4,17 @@ import 'package:stream_feed_flutter_core/stream_feed_flutter_core.dart'
     hide Image;
 
 class Avatar extends StatelessWidget {
+  Avatar({this.user, this.jsonKey});
+
   final User? user;
-  Avatar({this.user});
+  final String? jsonKey;
+
   @override
   Widget build(BuildContext context) {
     return user != null
         ? ClipOval(
             child: Image.network(
-              user!.data!['profile_image'] as String,
+              user!.data![jsonKey ?? 'profile_image'] as String,
               width: 30,
               height: 30,
               fit: BoxFit.cover,
