41319835517634bd9a61d6af246618e3af005989
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-06-07 10:21:25 -0400

    const

--- packages/stream_feed_flutter/lib/src/button.dart
+++ packages/stream_feed_flutter/lib/src/button.dart
@@ -11,7 +11,7 @@ class Button extends StatelessWidget {
 
   ///If it is a gradient or normal button
   final ButtonType type;
-  Button({
+  const Button({
     Key? key,
     required this.label,
     required this.onPressed,
