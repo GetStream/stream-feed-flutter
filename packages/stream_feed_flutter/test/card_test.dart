d310b2c0e7cb39068069eb550a9ab9937312388c
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-06-11 15:17:44 -0400

    mock MethodChannel url_launcher

--- packages/stream_feed_flutter/test/card_test.dart
+++ packages/stream_feed_flutter/test/card_test.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail_image_network/mocktail_image_network.dart';
 import 'package:stream_feed_flutter/src/card.dart';
 import 'package:stream_feed_flutter/stream_feed_flutter.dart';
 
-main() {
+void main() {
+  final logs = <String>[];
+  const channel = MethodChannel('plugins.flutter.io/url_launcher');
+
+  setUp(() {
+    channel.setMockMethodCallHandler((MethodCall methodCall) async {
+      if (methodCall.method == 'canLaunch') {
+        logs.add('canLaunch');
+        return true;
+      }
+
+      if (methodCall.method == 'launch') {
+        logs.add('launch');
+        return true;
+      }
+    });
+  });
   testWidgets('Card', (tester) async {
     await mockNetworkImages(() async {
+      final _url = 'http://example.com/';
       await tester.pumpWidget(MaterialApp(
           home: Scaffold(
         body: StreamFeedCard(
-          og: OpenGraphData(
-              url: 'dummyurl', images: [OgImage(secureUrl: 'dummyurl')]),
+          og: OpenGraphData(url: _url, images: [OgImage(secureUrl: _url)]),
         ),
       )));
 
@@ -23,6 +40,14 @@ main() {
 
       final image = find.byType(Image);
       expect(image, findsOneWidget);
+
+      await tester.tap(inkwell);
+      expect(logs,
+          ['canLaunch', 'launch']); //TODO: hmm find a better way to do this
     });
+  });
+
+  tearDown(() {
+    channel.setMockMethodCallHandler(null);
   });
 }
