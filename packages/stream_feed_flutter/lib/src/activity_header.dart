e7a778dcadf40fe8e7ebd54445a212681e670e8d
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-06-11 16:13:15 -0400

    onUserTap callback

--- packages/stream_feed_flutter/lib/src/activity_header.dart
+++ packages/stream_feed_flutter/lib/src/activity_header.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:stream_feed_flutter/src/typedefs.dart';
 import 'package:stream_feed_flutter/src/user_bar.dart';
 import 'package:stream_feed_flutter/stream_feed_flutter.dart';
 
@@ -6,13 +7,18 @@ class ActvityHeader extends StatelessWidget {
   const ActvityHeader(
       {Key? key,
       required this.activity,
+      this.onUserTap,
       this.activityKind = 'like'}); //TODO: enum that thing
   final EnrichedActivity activity;
+  final OnUserTap? onUserTap;
 
   ///Wether you want to display like activities or repost activities
   final String activityKind;
   @override
   Widget build(BuildContext context) {
-    return UserBar(timestamp: activity.time!, kind: activityKind);
+    return UserBar(
+        onUserTap: onUserTap,
+        timestamp: activity.time!,
+        kind: activityKind); //TODO: display what instead of null timestamp?
   }
 }
