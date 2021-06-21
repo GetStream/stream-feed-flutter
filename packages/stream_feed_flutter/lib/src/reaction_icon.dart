5eae7bc8de37b79e986dbb3db459e6056ffde620
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-06-21 11:46:37 -0400

    reaction icon: hover color option

--- packages/stream_feed_flutter/lib/src/reaction_icon.dart
+++ packages/stream_feed_flutter/lib/src/reaction_icon.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 
 class ReactionIcon extends StatelessWidget {
-  const ReactionIcon({Key? key, this.count, required this.icon, this.onTap})
-      : super(key: key);
+  const ReactionIcon({
+    Key? key,
+    this.count,
+    required this.icon,
+    this.onTap,
+    this.hoverColor = Colors.lightBlue,
+  }) : super(key: key);
   final int? count;
   final Widget icon;
   final VoidCallback? onTap;
+  final Color hoverColor;
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
+        hoverColor: hoverColor,
         borderRadius: BorderRadius.circular(18.0), //iconSize
         onTap: onTap,
         child: count != null && count! > 0
