6ef836b2ba5577520d7bc9bb43122fedd648fe63
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-06-21 09:56:37 -0400

    activity header: showSubtitle bool

--- packages/stream_feed_flutter/lib/src/activity_header.dart
+++ packages/stream_feed_flutter/lib/src/activity_header.dart
@@ -4,13 +4,16 @@ import 'package:stream_feed_flutter/src/user_bar.dart';
 import 'package:stream_feed_flutter/stream_feed_flutter.dart';
 
 class ActivityHeader extends StatelessWidget {
-  const ActivityHeader(
-      {Key? key,
-      required this.activity,
-      this.onUserTap,
-      this.activityKind = 'like'}); //TODO: enum that thing
+  const ActivityHeader({
+    Key? key,
+    required this.activity,
+    this.onUserTap,
+    this.activityKind = 'like', //TODO: enum that thing
+    this.showSubtitle = true,
+  });
   final EnrichedActivity activity;
   final OnUserTap? onUserTap;
+  final bool showSubtitle;
 
   ///Wether you want to display like activities or repost activities
   final String activityKind;
@@ -21,10 +24,11 @@ class ActivityHeader extends StatelessWidget {
     final user =
         User.fromJson(serializedActor as Map<String, dynamic>); //TODO: ugly
     return UserBar(
-      user: user,
-      onUserTap: onUserTap,
-      timestamp: activity.time!,
-      kind: activityKind,
-    ); //TODO: display what instead of null timestamp?
+        user: user,
+        onUserTap: onUserTap,
+        timestamp: activity.time!,
+        kind: activityKind,
+        showSubtitle:
+            showSubtitle); //TODO: display what instead of null timestamp?
   }
 }
