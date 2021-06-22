a7c2d38c0bd469af18ac6837ff11f4583bb3debf
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-06-22 08:45:26 -0400

    fix renderflex overflow activity content: expanded

--- packages/stream_feed_flutter/lib/src/widgets/activity/content.dart
+++ packages/stream_feed_flutter/lib/src/widgets/activity/content.dart
@@ -25,24 +25,26 @@ class ActivityContent extends StatelessWidget {
         ? detector.parseText(
             EnrichableField.serialize(activityObject) as String) //TODO: ugly
         : <TaggedText?>[];
-    return Column(
-      children: [
-        Wrap(
-          //TODO: move to Text.rich(WidgetSpans)
-          children: taggedText
-              .map((it) => InteractiveText(
-                    //TODO: not interactive in case of a response
-                    tagged: it,
-                    onHashtagTap: onHashtagTap,
-                    onMentionTap: onMentionTap,
-                  ))
-              .toList(),
-        ),
-        //TODO: handle Video + Audio + Gallery
-        if (attachments != null)
-          StreamFeedCard(
-              og: OpenGraphData.fromJson(attachments as Map<String, dynamic>))
-      ],
+    return Expanded(
+      child: Column(
+        children: [
+          Wrap(
+            //TODO: move to Text.rich(WidgetSpans)
+            children: taggedText
+                .map((it) => InteractiveText(
+                      //TODO: not interactive in case of a response
+                      tagged: it,
+                      onHashtagTap: onHashtagTap,
+                      onMentionTap: onMentionTap,
+                    ))
+                .toList(),
+          ),
+          //TODO: handle Video + Audio + Gallery
+          if (attachments != null)
+            StreamFeedCard(
+                og: OpenGraphData.fromJson(attachments as Map<String, dynamic>))
+        ],
+      ),
     );
   }
 }
