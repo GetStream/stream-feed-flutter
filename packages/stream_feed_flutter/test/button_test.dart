91e4f749305edb9750a9041926d5181e234f9cb9
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-06-22 09:56:24 -0400

    refactor button related tests into one file

--- packages/stream_feed_flutter/test/button_test.dart
+++ packages/stream_feed_flutter/test/button_test.dart
@@ -1,34 +1,307 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
+import 'package:mocktail_image_network/mocktail_image_network.dart';
+import 'package:stream_feed_flutter/src/widgets/buttons/buttons.dart';
+import 'package:stream_feed_flutter/src/widgets/buttons/reaction.dart';
+import 'package:stream_feed_flutter/src/widgets/buttons/reaction_list.dart';
 import 'package:stream_feed_flutter/src/widgets/buttons/text.dart';
+import 'package:stream_feed_flutter/src/widgets/icons.dart';
+import 'package:stream_feed_flutter/src/widgets/user/avatar.dart';
 import 'package:stream_feed_flutter/stream_feed_flutter.dart';
-// import 'package:test/test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import 'mock.dart';
 
 void main() {
-  group('Button', () {
-    testGoldens('button', (tester) async {
+  testWidgets('ReactionList', (tester) async {
+    await mockNetworkImages(() async {
+      var pressedHashtags = <String?>[];
+      var pressedMentions = <String?>[];
+      var pressedReactions = <Reaction?>[];
+      var pressedUsers = <User?>[];
+      final reactions = [
+        Reaction(
+          createdAt: DateTime.now(),
+          kind: 'comment',
+          data: {
+            'text':
+                'Woohoo Snowboarding is awesome! #snowboarding #winter @sacha',
+          },
+        ),
+        Reaction(
+          createdAt: DateTime.now(),
+          kind: 'comment',
+          data: {
+            'text': 'Ikr! #vacations #winter @sahil',
+          },
+        ),
+      ];
+      await tester.pumpWidget(MaterialApp(
+          home: Scaffold(
+        body: ReactionList(
+            onUserTap: (user) => pressedUsers.add(user),
+            onReactionTap: (reaction) => pressedReactions.add(reaction),
+            onMentionTap: (mention) => pressedMentions.add(mention),
+            onHashtagTap: (hashtag) => pressedHashtags.add(hashtag),
+            reactions: reactions),
+      )));
+
+      final avatar = find.byType(Avatar);
+
+      expect(avatar, findsNWidgets(2));
+      final richtexts = tester.widgetList<Text>(find.byType(Text));
+
+      expect(richtexts.toList().map((e) => e.data), [
+        'a moment ago',
+        'Woohoo ',
+        'Snowboarding ',
+        'is ',
+        'awesome! ',
+        ' #snowboarding',
+        ' #winter',
+        ' @sacha',
+        'a moment ago',
+        'Ikr! ',
+        ' #vacations',
+        ' #winter',
+        ' @sahil'
+      ]);
+
+      await tester.tap(find.widgetWithText(InkWell, ' #winter').first);
+      await tester.tap(find.widgetWithText(InkWell, ' @sacha').first);
+      await tester.tap(find.widgetWithText(InkWell, ' #vacations').first);
+      await tester.tap(find.widgetWithText(InkWell, ' @sahil').first);
+      final firstReaction = find.byType(InkWell).first;
+      final firstReactionUser = find.byType(Avatar).first;
+      await tester.tap(firstReaction);
+      await tester.tap(firstReactionUser);
+      expect(pressedHashtags, ['winter', 'vacations']);
+      expect(pressedMentions, ['sacha', 'sahil']);
+      expect(pressedReactions, [reactions.first]);
+      expect(pressedUsers, [reactions.first.user]);
+    });
+  });
+  testWidgets('LikeButton', (tester) async {
+    await tester.pumpWidget(MaterialApp(
+        home: Scaffold(
+      body: LikeButton(
+        activity:
+            EnrichedActivity(), //TODO: put actual fields in this, notes: look into checks in llc reactions
+        // .add and .delete
+        reaction: Reaction(kind: 'like', childrenCounts: {
+          'like': 3
+        }, ownChildren: {
+          'like': [
+            Reaction(
+              kind: 'like',
+            )
+          ]
+        }),
+      ),
+    )));
+
+    final icon = find.byType(StreamSvgIcon);
+    final activeIcon = tester.widget<StreamSvgIcon>(icon);
+    final count = find.text('3');
+    expect(count, findsOneWidget);
+    expect(activeIcon.assetName, StreamSvgIcon.loveActive().assetName);
+  });
+
+  testWidgets('RepostButton', (tester) async {
+    await tester.pumpWidget(MaterialApp(
+        home: Scaffold(
+      body: RepostButton(
+        activity:
+            EnrichedActivity(), //TODO: put actual fields in this, notes: look into checks in llc reactions
+        // .add and .delete
+        reaction: Reaction(kind: 'repost', childrenCounts: {
+          'repost': 3
+        }, ownChildren: {
+          'repost': [
+            Reaction(
+              kind: 'repost',
+            )
+          ]
+        }),
+      ),
+    )));
+
+    final icon = find.byType(StreamSvgIcon);
+    final activeIcon = tester.widget<StreamSvgIcon>(icon);
+    final count = find.text('3');
+    expect(count, findsOneWidget);
+    expect(activeIcon.color, Colors.blue);
+  });
+
+  group('ReactionToggleIcon', () {
+    const kind = 'like';
+    const count = 1300;
+    final inactiveIcon = StreamSvgIcon.loveInactive();
+    final activeIcon = StreamSvgIcon.loveActive();
+    const foreignId = 'like:300';
+    const activityId = 'activityId';
+    const feedGroup = 'timeline:300';
+    final activity = EnrichedActivity(id: activityId, foreignId: foreignId);
+    const reaction = Reaction(id: 'id', kind: kind, activityId: activityId);
+    const userId = 'user:300';
+    final withoutOwnReactions = ReactionToggleIcon(
+        activity: activity,
+        kind: kind,
+        count: count,
+        feedGroup: feedGroup,
+        inactiveIcon: inactiveIcon,
+        activeIcon: activeIcon);
+    final withOwnReactions = ReactionToggleIcon(
+        activity: activity,
+        kind: kind,
+        count: count,
+        feedGroup: feedGroup,
+        ownReactions: [reaction],
+        inactiveIcon: inactiveIcon,
+        activeIcon: activeIcon);
+    group('widget test', () {
+      testWidgets('withoutOwnReactions: onAddReaction', (tester) async {
+        final mockClient = MockStreamFeedClient();
+        final mockReactions = MockReactions();
+        final mockStreamAnalytics = MockStreamAnalytics();
+        when(() => mockClient.reactions).thenReturn(mockReactions);
+
+        const label = kind;
+        final engagement = Engagement(
+            content: Content(foreignId: FeedId.fromId(activity.foreignId)),
+            label: label,
+            feedId: FeedId.fromId(feedGroup));
+
+        when(() => mockReactions.add(
+              kind,
+              activityId,
+            )).thenAnswer((_) async => reaction);
+
+        when(() => mockStreamAnalytics.trackEngagement(engagement))
+            .thenAnswer((_) async => Future.value());
+
+        await tester.pumpWidget(MaterialApp(
+            home: Scaffold(
+          body: StreamFeedCore(
+              analyticsClient: mockStreamAnalytics,
+              client: mockClient,
+              child: withoutOwnReactions),
+        )));
+        final reactionIcon = find.byType(ReactionToggleIcon);
+        expect(reactionIcon, findsOneWidget);
+        await tester.tap(reactionIcon);
+        verify(() => mockClient.reactions.add(
+              kind,
+              activityId,
+            )).called(1);
+        verify(() => mockStreamAnalytics.trackEngagement(engagement)).called(1);
+      });
+
+      testWidgets('withOwnReactions: onRemoveReaction', (tester) async {
+        final mockClient = MockStreamFeedClient();
+        final mockReactions = MockReactions();
+        final mockStreamAnalytics = MockStreamAnalytics();
+        when(() => mockClient.reactions).thenReturn(mockReactions);
+
+        const label = kind;
+        final engagement = Engagement(
+            content: Content(foreignId: FeedId.fromId(activity.foreignId)),
+            label: 'un$label',
+            feedId: FeedId.fromId(feedGroup));
+
+        when(() => mockReactions.delete(reaction.id!))
+            .thenAnswer((_) async => reaction);
+
+        when(() => mockStreamAnalytics.trackEngagement(engagement))
+            .thenAnswer((_) async => Future.value());
+
+        await tester.pumpWidget(MaterialApp(
+            home: Scaffold(
+          body: StreamFeedCore(
+              analyticsClient: mockStreamAnalytics,
+              client: mockClient,
+              child: withOwnReactions),
+        )));
+        final reactionIcon = find.byType(ReactionToggleIcon);
+        expect(reactionIcon, findsOneWidget);
+        await tester.tap(reactionIcon);
+        verify(() => mockClient.reactions.delete(reaction.id!)).called(1);
+        verify(() => mockStreamAnalytics.trackEngagement(engagement)).called(1);
+      });
+    });
+
+    testGoldens('golden', (tester) async {
       final builder = GoldenBuilder.grid(columns: 2, widthToHeightRatio: 0.5)
-        ..addScenario(
-            'Button primary',
-            StyledTextButton(
-              label: 'Follow',
-              onPressed: () {},
-              type: ButtonType.primary,
-            ))
-        ..addScenario(
-            'Button faded',
-            StyledTextButton(
-              label: 'Following',
-              onPressed: () {},
-              type: ButtonType.faded,
-            ));
+        ..addScenario('without own reactions', withoutOwnReactions)
+        ..addScenario('with own reactions', withOwnReactions);
 
       await tester.pumpWidgetBuilder(
         builder.build(),
-        surfaceSize: const Size(200, 150),
+        surfaceSize: const Size(250, 100),
       );
-      await screenMatchesGolden(tester, 'buttons_grid');
+      await screenMatchesGolden(tester, 'reaction_toggle_icon_grid');
     });
+  });
+
+  group('ReactionIcon', () {
+    testWidgets('onTap', (tester) async {
+      var tapped = 0;
+      await tester.pumpWidget(MaterialApp(
+          home: Scaffold(
+        body: ReactionIcon(
+          icon: StreamSvgIcon.repost(),
+          count: 23,
+          onTap: () => tapped++,
+        ),
+      )));
+
+      final icon = find.byType(StreamSvgIcon);
+      final activeIcon = tester.widget<StreamSvgIcon>(icon);
+      final count = find.text('23');
+      expect(count, findsOneWidget);
+      expect(activeIcon.assetName, StreamSvgIcon.repost().assetName);
+
+      await tester.tap(find.byType(InkWell));
+      expect(tapped, 1);
+    });
+
+    testGoldens('repost golden', (tester) async {
+      await tester.pumpWidgetBuilder(
+        Center(
+          child: ReactionIcon(
+            icon: StreamSvgIcon.repost(),
+            count: 23,
+          ),
+        ),
+        surfaceSize: const Size(100, 75),
+      );
+      await screenMatchesGolden(tester, 'repost');
+    });
+  });
+
+  testGoldens('StyledTextButton', (tester) async {
+    final builder = GoldenBuilder.grid(columns: 2, widthToHeightRatio: 0.5)
+      ..addScenario(
+          'Button primary',
+          StyledTextButton(
+            label: 'Follow',
+            onPressed: () {},
+            type: ButtonType.primary,
+          ))
+      ..addScenario(
+          'Button faded',
+          StyledTextButton(
+            label: 'Following',
+            onPressed: () {},
+            type: ButtonType.faded,
+          ));
+
+    await tester.pumpWidgetBuilder(
+      builder.build(),
+      surfaceSize: const Size(200, 150),
+    );
+    await screenMatchesGolden(tester, 'buttons_grid');
   });
 }
