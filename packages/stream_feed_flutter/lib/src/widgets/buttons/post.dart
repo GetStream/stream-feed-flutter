ee8b2950945ea7fd66491ec4f0f04265deb80006
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-07-15 15:11:12 -0400

    fix count for comments too

--- packages/stream_feed_flutter/lib/src/widgets/buttons/post.dart
+++ packages/stream_feed_flutter/lib/src/widgets/buttons/post.dart
@@ -13,6 +13,7 @@ class PostButton extends StatelessWidget {
     Key? key,
     this.feedGroup = 'user',
     required this.activity,
+    this.reaction,
     this.iconSize = 14,
   }) : super(key: key);
 
@@ -22,34 +23,45 @@ class PostButton extends StatelessWidget {
   /// The activity to post to the feed.
   final EnrichedActivity activity;
 
+  final Reaction? reaction;
+
   /// The size of the icon to display.
   final double iconSize;
 
+  int? get count =>
+      reaction?.childrenCounts?['comment'] ??
+      activity.reactionCounts?['comment'];
+
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      splashRadius: iconSize + 4,
-      iconSize: iconSize,
-      hoverColor: Colors.blue.shade100,
-      onPressed: () {
-        showDialog<void>(
-          //TODO: switch (await showDialog<Delete/Register>
-          context: context,
-          builder: (_) {
-            return MediaQuery(
-                data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
-                child: StreamFeedCore(
-                  //TODO: there might be a better way to do this
-                  client: StreamFeedCore.of(context).client,
-                  child: AlertDialogComment(
-                    activity: activity,
-                    feedGroup: feedGroup,
-                  ),
-                ));
+    return Row(
+      children: [
+        IconButton(
+          splashRadius: iconSize + 4,
+          iconSize: iconSize,
+          hoverColor: Colors.blue.shade100,
+          onPressed: () {
+            showDialog<void>(
+              //TODO: switch (await showDialog<Delete/Register>
+              context: context,
+              builder: (_) {
+                return MediaQuery(
+                    data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+                    child: StreamFeedCore(
+                      //TODO: there might be a better way to do this
+                      client: StreamFeedCore.of(context).client,
+                      child: AlertDialogComment(
+                        activity: activity,
+                        feedGroup: feedGroup,
+                      ),
+                    ));
+              },
+            );
           },
-        );
-      },
-      icon: StreamSvgIcon.post(),
+          icon: StreamSvgIcon.post(),
+        ),
+        if (count != null && count! > 0) Text('${count!}')
+      ],
     );
   }
 }
