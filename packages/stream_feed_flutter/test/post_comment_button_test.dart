c6414ae4b0d8535e82a316c34db3bb7e31da93dd
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-07-23 14:07:20 -0400

    rename test

--- packages/stream_feed_flutter/test/post_comment_button_test.dart
+++ packages/stream_feed_flutter/test/post_comment_button_test.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:stream_feed_flutter/src/widgets/comment/button.dart';
 import 'package:stream_feed_flutter/src/widgets/dialogs/dialogs.dart';
 import 'package:mocktail/mocktail.dart';
 
 import 'mock.dart';
 
 main() {
-  testWidgets('PostCommentButton', (tester) async {
+  //TODO: PostCommentButton
+  testWidgets('MediasAction', (tester) async {
     final imagePicker = MockImagePicker();
     when(() => imagePicker.pickImage(source: ImageSource.gallery))
         .thenAnswer((_) async => XFile("dummyPath"));
