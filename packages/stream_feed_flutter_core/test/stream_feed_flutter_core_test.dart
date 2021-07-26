88c7a1a85723325d3780f96f242a64b9d80e6d5c
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-07-26 16:37:15 -0400

    throw error if StreamFeedCore is not in the tree

--- packages/stream_feed_flutter_core/test/stream_feed_flutter_core_test.dart
+++ packages/stream_feed_flutter_core/test/stream_feed_flutter_core_test.dart
@@ -4,6 +4,20 @@ import 'package:stream_feed_flutter_core/stream_feed_flutter_core.dart';
 
 import 'mocks.dart';
 
+class TestWidget extends StatefulWidget {
+  const TestWidget({Key? key}) : super(key: key);
+
+  @override
+  _TestWidgetState createState() => _TestWidgetState();
+}
+
+class _TestWidgetState extends State<TestWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
 void main() {
   testWidgets(
     'should render StreamFeedCore if both client and child is provided',
@@ -21,6 +35,20 @@ void main() {
 
       expect(find.byKey(streamFeedCoreKey), findsOneWidget);
       expect(find.byKey(childKey), findsOneWidget);
+    },
+  );
+  testWidgets(
+    'throw an error if StreamFeedCore is not in the tree',
+    (tester) async {
+      final childKey = GlobalKey();
+
+      await tester.pumpWidget(TestWidget(key: childKey));
+
+      expect(
+          () => StreamFeedCore.of(childKey.currentState!.context),
+          throwsA(predicate<AssertionError>(
+              (e) => e.message == 'You must have a StreamFeed widget at the top of your widget tree')));
+      // expect(find.byKey(childKey), findsOneWidget);
     },
   );
 }
