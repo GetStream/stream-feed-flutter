528bb206cc7ef96171c350f22b4603ad706a4415
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-07-26 17:01:02 -0400

    test on add reaction in core

--- packages/stream_feed_flutter_core/test/mocks.dart
+++ packages/stream_feed_flutter_core/test/mocks.dart
@@ -3,6 +3,12 @@ import 'package:stream_feed/stream_feed.dart';
 
 class MockLogger extends Mock implements Logger {}
 
+class MockStreamFeedClient extends Mock implements StreamFeedClient {}
+
+class MockReactions extends Mock implements ReactionsClient {}
+
+class MockStreamAnalytics extends Mock implements StreamAnalytics {}
+
 class MockClient extends Mock implements StreamFeedClient {
   final Logger logger = MockLogger();
 
