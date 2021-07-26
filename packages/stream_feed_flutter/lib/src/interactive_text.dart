a8cd6006274260c425b6e63c0ca8db9e58ace9e4
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-07-26 17:38:49 -0400

    make tagged non nullable

--- packages/stream_feed_flutter/lib/src/interactive_text.dart
+++ packages/stream_feed_flutter/lib/src/interactive_text.dart
@@ -1,44 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:stream_feed_flutter/src/typedefs.dart';
-import 'package:stream_feed_flutter/src/utils/tag_detector.dart';
-
 import 'package:stream_feed_flutter/src/utils/extensions.dart';
+import 'package:stream_feed_flutter/src/utils/tag_detector.dart';
 
 class InteractiveText extends StatelessWidget {
   final OnMentionTap? onMentionTap;
   final OnHashtagTap? onHashtagTap;
-  final TaggedText? tagged;
+  final TaggedText tagged;
   const InteractiveText({
-    this.tagged,
+    Key? key,
+    required this.tagged,
     this.onHashtagTap,
     this.onMentionTap,
-  });
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    if (tagged != null) {
-      switch (tagged!.tag) {
-        case Tag.normalText:
-          return Text('${tagged!.text!} ', style: tagged!.tag.style());
-        case Tag.hashtag:
-          return InkWell(
-            onTap: () {
-              onHashtagTap?.call(tagged!.text?.trim().replaceFirst('#', ''));
-            },
-            child: Text(tagged!.text!, style: tagged!.tag.style()),
-          );
-        case Tag.mention:
-          return InkWell(
-            onTap: () {
-              onMentionTap?.call(tagged!.text?.trim().replaceFirst('@', ''));
-            },
-            child: Text(tagged!.text!, style: tagged!.tag.style()),
-          );
-        default:
-          return Text(tagged!.text!, style: tagged!.tag.style());
-      }
-    } else {
-      return Offstage();
+    switch (tagged.tag) {
+      case Tag.normalText:
+        return Text('${tagged.text} ', style: tagged.tag.style());
+      case Tag.hashtag:
+        return InkWell(
+          onTap: () {
+            onHashtagTap?.call(tagged.text.trim().replaceFirst('#', ''));
+          },
+          child: Text(tagged.text, style: tagged.tag.style()),
+        );
+      case Tag.mention:
+        return InkWell(
+          onTap: () {
+            onMentionTap?.call(tagged.text.trim().replaceFirst('@', ''));
+          },
+          child: Text(tagged.text, style: tagged.tag.style()),
+        );
+      default:
+        return Text(tagged.text, style: tagged.tag.style());
     }
   }
 }
