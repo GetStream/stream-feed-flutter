ebec8b3b0744d5d4cf47ec9e133837e25b8e0da8
groovinchip <groovinchip@gmail.com>
2021-08-12 10:37:06 -0400

    docs: update docs for foreign_id_time_pair.dart

--- packages/stream_feed/lib/src/core/models/foreign_id_time_pair.dart
+++ packages/stream_feed/lib/src/core/models/foreign_id_time_pair.dart
@@ -1,15 +1,15 @@
 import 'package:equatable/equatable.dart';
 import 'package:stream_feed/src/core/util/extension.dart';
 
-///
+/// Wraps a foreign id with a time
 class ForeignIdTimePair extends Equatable {
-  ///
+  /// Builds a [ForeignIdTimePair].
   const ForeignIdTimePair(this.foreignID, this.time);
 
-  ///
+  /// The foreign id of an activity
   final String foreignID;
 
-  ///
+  /// The time of the activity
   final DateTime time;
 
   ///
