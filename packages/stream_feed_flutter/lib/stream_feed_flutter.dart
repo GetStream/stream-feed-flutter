9b82a66f050e65ef3ba26b90b320bd3256dcf5ab
groovinchip <groovinchip@gmail.com>
2021-08-20 15:50:10 -0400

    export more widgets for flutter sdk

--- packages/stream_feed_flutter/lib/stream_feed_flutter.dart
+++ packages/stream_feed_flutter/lib/stream_feed_flutter.dart
@@ -2,5 +2,7 @@ library stream_feed_flutter;
 
 export 'package:stream_feed_flutter_core/stream_feed_flutter_core.dart'
     hide FlatFeed;
+
+export 'src/widgets/comment/field.dart';
 export 'src/widgets/pages/flat_activity_list.dart';
 export 'src/widgets/user/avatar.dart';
