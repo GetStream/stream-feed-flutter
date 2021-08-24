f7e040a89299b02945e23d2413fd53c7349c7fe7
groovinchip <groovinchip@gmail.com>
2021-08-24 15:36:01 -0400

    chore: added generic bounds for personalizedFeed

--- packages/stream_feed/lib/src/core/models/personalized_feed.dart
+++ packages/stream_feed/lib/src/core/models/personalized_feed.dart
@@ -7,21 +7,53 @@ part 'personalized_feed.g.dart';
 /// A personalized feed for a single user.
 ///
 /// In other words, a feed of based on user's activities.
-@JsonSerializable(createToJson: true)
-class PersonalizedFeed extends Paginated<EnrichedActivity> {
+@JsonSerializable(createToJson: true, genericArgumentFactories: true)
+class PersonalizedFeed<A, Ob, T, Or>
+    extends Paginated<EnrichedActivity<A, Ob, T, Or>> {
   /// Builds a [PaginatedReactions].
   const PersonalizedFeed({
     required this.version,
     required this.offset,
     required this.limit,
     String? next,
-    List<EnrichedActivity>? results,
+    List<EnrichedActivity<A, Ob, T, Or>>? results,
     String? duration,
   }) : super(next, results, duration);
 
   /// Deserialize json to [PaginatedReactions]
-  factory PersonalizedFeed.fromJson(Map<String, dynamic> json) =>
-      _$PersonalizedFeedFromJson(json);
+  factory PersonalizedFeed.fromJson(
+    Map<String, dynamic> json,
+    A Function(Object? json)? fromJsonA,
+    Ob Function(Object? json)? fromJsonOb,
+    T Function(Object? json)? fromJsonT,
+    Or Function(Object? json)? fromJsonOr,
+  ) =>
+      _$PersonalizedFeedFromJson<A, Ob, T, Or>(
+        json,
+        fromJsonA ??
+            (jsonA) => (A == User)
+                ? User.fromJson(jsonA! as Map<String, dynamic>) as A
+                : jsonA as A,
+        fromJsonOb ??
+            (jsonOb) => (Ob == CollectionEntry)
+                ? CollectionEntry.fromJson(jsonOb! as Map<String, dynamic>)
+                    as Ob
+                : jsonOb as Ob,
+        fromJsonT ??
+            (jsonT) => (T == Activity)
+                ? Activity.fromJson(jsonT! as Map<String, dynamic>) as T
+                : jsonT as T,
+        fromJsonOr ??
+            (jsonOr) {
+              if (Or == User) {
+                return User.fromJson(jsonOr! as Map<String, dynamic>) as Or;
+              } else if (Or == Reaction) {
+                return Reaction.fromJson(jsonOr! as Map<String, dynamic>) as Or;
+              } else {
+                return jsonOr as Or;
+              }
+            },
+      );
 
   @override
   List<Object?> get props => [...super.props, version, offset, limit];
@@ -35,5 +67,11 @@ class PersonalizedFeed extends Paginated<EnrichedActivity> {
   final int limit;
 
   /// Serialize to json
-  Map<String, dynamic> toJson() => _$PersonalizedFeedToJson(this);
+  Map<String, dynamic> toJson(
+    Object? Function(A value) toJsonA,
+    Object? Function(Ob value) toJsonOb,
+    Object? Function(T value) toJsonT,
+    Object? Function(Or value) toJsonOr,
+  ) =>
+      _$PersonalizedFeedToJson(this, toJsonA, toJsonOb, toJsonT, toJsonOr);
 }
--- packages/stream_feed/lib/src/core/models/personalized_feed.g.dart
+++ packages/stream_feed/lib/src/core/models/personalized_feed.g.dart
@@ -6,8 +6,14 @@ part of 'personalized_feed.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-PersonalizedFeed _$PersonalizedFeedFromJson(Map json) {
-  return PersonalizedFeed(
+PersonalizedFeed<A, Ob, T, Or> _$PersonalizedFeedFromJson<A, Ob, T, Or>(
+  Map json,
+  A Function(Object? json) fromJsonA,
+  Ob Function(Object? json) fromJsonOb,
+  T Function(Object? json) fromJsonT,
+  Or Function(Object? json) fromJsonOr,
+) {
+  return PersonalizedFeed<A, Ob, T, Or>(
     version: json['version'] as String,
     offset: json['offset'] as int,
     limit: json['limit'] as int,
@@ -17,24 +23,30 @@ PersonalizedFeed _$PersonalizedFeedFromJson(Map json) {
             (e as Map?)?.map(
               (k, e) => MapEntry(k as String, e),
             ),
-            (value) => value,
-            (value) => value,
-            (value) => value,
-            (value) => value))
+            (value) => fromJsonA(value),
+            (value) => fromJsonOb(value),
+            (value) => fromJsonT(value),
+            (value) => fromJsonOr(value)))
         .toList(),
     duration: json['duration'] as String?,
   );
 }
 
-Map<String, dynamic> _$PersonalizedFeedToJson(PersonalizedFeed instance) =>
+Map<String, dynamic> _$PersonalizedFeedToJson<A, Ob, T, Or>(
+  PersonalizedFeed<A, Ob, T, Or> instance,
+  Object? Function(A value) toJsonA,
+  Object? Function(Ob value) toJsonOb,
+  Object? Function(T value) toJsonT,
+  Object? Function(Or value) toJsonOr,
+) =>
     <String, dynamic>{
       'next': instance.next,
       'results': instance.results
           ?.map((e) => e.toJson(
-                (value) => value,
-                (value) => value,
-                (value) => value,
-                (value) => value,
+                (value) => toJsonA(value),
+                (value) => toJsonOb(value),
+                (value) => toJsonT(value),
+                (value) => toJsonOr(value),
               ))
           .toList(),
       'duration': instance.duration,
