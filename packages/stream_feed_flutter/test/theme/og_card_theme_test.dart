d670fac6515013b7f6da6e80f2dfb01cb55001d6
groovinchip <groovinchip@gmail.com>
2021-08-31 12:39:21 -0400

    test: add og_card_theme_test.dart

---
+++ packages/stream_feed_flutter/test/theme/og_card_theme_test.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:stream_feed_flutter/stream_feed_flutter.dart';
+
+void main() {
+  test('OgCardThemeData copyWith, ==, hashCode basics', () {
+    expect(const OgCardThemeData(),
+        const OgCardThemeData().copyWith());
+    expect(const OgCardThemeData().hashCode,
+        const OgCardThemeData().copyWith().hashCode);
+  });
+
+  test('OgCardThemeData lerps halfway', () {
+    expect(
+        const OgCardThemeData()
+            .lerp(_ogCardThemeDefault, _ogCardThemeFullLerp, 0.5),
+        _ogCardThemeMidLerp);
+  });
+}
+
+const _ogCardThemeDefault = OgCardThemeData(
+  titleTextStyle: TextStyle(
+    color: Color(0xff007aff),
+    fontSize: 14,
+    overflow: TextOverflow.ellipsis,
+  ),
+  descriptionTextStyle: TextStyle(
+    color: Color(0xff364047),
+    fontSize: 13,
+    overflow: TextOverflow.ellipsis,
+  ),
+);
+
+const _ogCardThemeMidLerp = OgCardThemeData(
+  titleTextStyle: TextStyle(
+    color: Color(0xff7a5e9a),
+    fontSize: 14,
+    overflow: TextOverflow.ellipsis,
+  ),
+  descriptionTextStyle: TextStyle(
+    color: Color(0xff3c4144),
+    fontSize: 13,
+    overflow: TextOverflow.ellipsis,
+  ),
+);
+
+final _ogCardThemeFullLerp = OgCardThemeData(
+  titleTextStyle: const TextStyle(
+    color: Colors.red,
+    fontSize: 14,
+    overflow: TextOverflow.ellipsis,
+  ),
+  descriptionTextStyle: TextStyle(
+    color: Colors.grey.shade800,
+    fontSize: 13,
+    overflow: TextOverflow.ellipsis,
+  ),
+);
