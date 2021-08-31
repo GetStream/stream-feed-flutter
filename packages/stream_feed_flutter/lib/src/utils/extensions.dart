2f5a1ee9056bb250ad968bcd8d1a4d3043e39c13
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-08-31 13:09:14 -0400

    delete unused tag style related code

--- packages/stream_feed_flutter/lib/src/utils/extensions.dart
+++ packages/stream_feed_flutter/lib/src/utils/extensions.dart
@@ -4,13 +4,6 @@ import 'package:stream_feed_flutter/src/utils/tag_detector.dart';
 part 'constants.dart';
 
 /// TODO: document me
-extension ListTaggedTextX on List<TaggedText> {
-  /// TODO: document me
-  List<TextSpan> spans() =>
-      map((e) => TextSpan(text: e.text, style: e.tag.style())).toList();
-}
-
-/// TODO: document me
 extension TagX on Tag {
   // TODO: Improve all regex
   // eg -> @#sahil (Detects as normal text)
@@ -27,21 +20,5 @@ extension TagX on Tag {
         Tag.hashtag: 'hashtag',
         Tag.mention: 'mention',
         Tag.normalText: 'normalText',
-      }[this]!;
-
-  /// TODO: document me
-  TextStyle style() => <Tag, TextStyle>{
-        Tag.hashtag: const TextStyle(
-          color: Color(0xff0076ff),
-          fontSize: 14,
-        ),
-        Tag.mention: const TextStyle(
-          color: Color(0xff0076ff),
-          fontSize: 14,
-        ),
-        Tag.normalText: const TextStyle(
-          color: Colors.black,
-          fontSize: 14,
-        ),
       }[this]!;
 }
