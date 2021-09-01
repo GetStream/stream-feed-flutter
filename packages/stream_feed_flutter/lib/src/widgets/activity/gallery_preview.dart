91108ab5c9cb8015c6482dc839399f6bd9ac432c
groovinchip <groovinchip@gmail.com>
2021-09-01 16:21:35 -0400

    feat(ui): fix image loading issue in gallery_preview.dart

--- packages/stream_feed_flutter/lib/src/widgets/activity/gallery_preview.dart
+++ packages/stream_feed_flutter/lib/src/widgets/activity/gallery_preview.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:stream_feed_flutter/src/widgets/circular_progress_indicator.dart';
 
 /// A widget that displays image previews
 class GalleryPreview extends StatelessWidget {
@@ -20,9 +21,26 @@ class GalleryPreview extends StatelessWidget {
         itemCount: urls.length,
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
+          crossAxisSpacing: 8,
+          mainAxisSpacing: 8,
         ),
         itemBuilder: (context, index) {
-          return Image.network(urls[index]);
+          return Image.network(
+            urls[index],
+            height: 100,
+            width: 100,
+            loadingBuilder: (
+              BuildContext context,
+              Widget child,
+              ImageChunkEvent? loadingProgress,
+            ) {
+              return StreamCircularProgressIndicator(
+                loadingProgress: loadingProgress,
+                child: child,
+              );
+            },
+            fit: BoxFit.fill,
+          );
         },
       ),
     );
