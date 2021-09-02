6970c5fbc3782592fe7d3bd8c45555662fd378e3
groovinchip <groovinchip@gmail.com>
2021-09-02 14:17:30 -0400

    feat(ui): start fleshing out fullscreen_media.dart

---
+++ packages/stream_feed_flutter/lib/src/media/fullscreen_media.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:stream_feed_flutter/stream_feed_flutter.dart';
+
+// ignore_for_file: cascade_invocations
+
+/// Displays [Media] from a Feed in fullscreen.
+class FullscreenMedia extends StatefulWidget {
+  /// Builds a [FullscreenMedia].
+  const FullscreenMedia({
+    Key? key,
+    required this.media,
+    this.startIndex = 0,
+  }) : super(key: key);
+
+  /// The media to display.
+  ///
+  /// Can be audio, images, or videos.
+  final List<Media> media;
+
+  /// The first index of the media being shown.
+  final int startIndex;
+
+  @override
+  _FullscreenMediaState createState() => _FullscreenMediaState();
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(IterableProperty<Media>('media', media));
+    properties.add(IntProperty('startIndex', startIndex));
+  }
+}
+
+class _FullscreenMediaState extends State<FullscreenMedia>
+    with SingleTickerProviderStateMixin {
+  bool _optionsShown = true;
+
+  late final AnimationController _controller;
+  late final PageController _pageController;
+
+  late int _currentPage;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
+    _pageController = PageController(initialPage: widget.startIndex);
+    _currentPage = widget.startIndex;
+  }
+
+  @override
+  void dispose() {
+    //
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Stack(
+        children: [],
+      ),
+    );
+  }
+}
