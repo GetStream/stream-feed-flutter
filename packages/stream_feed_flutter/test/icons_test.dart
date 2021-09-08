d583b2ac2cdfcba66d379060b403835c85546544
groovinchip <groovinchip@gmail.com>
2021-09-08 10:38:30 -0400

    delete unused icon; formatting in golden test

--- packages/stream_feed_flutter/test/icons_test.dart
+++ packages/stream_feed_flutter/test/icons_test.dart
@@ -80,8 +80,8 @@ void main() {
     testGoldens('close', (tester) async {
       await tester.pumpWidgetBuilder(
         Center(
-            child:
-                Container(color: Colors.black, child: StreamSvgIcon.close())),
+          child: Container(color: Colors.black, child: StreamSvgIcon.close()),
+        ),
         surfaceSize: const Size(50, 50),
       );
       await screenMatchesGolden(tester, 'close');
