89512eb55b151f06f2f41e0deeb5f2afabb860fa
groovinchip <groovinchip@gmail.com>
2021-09-08 11:55:03 -0400

    fix: update fullscreen_media_test.dart

--- packages/stream_feed_flutter/test/fullscreen_media_test.dart
+++ packages/stream_feed_flutter/test/fullscreen_media_test.dart
@@ -3,24 +3,48 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:stream_feed_flutter/stream_feed_flutter.dart';
 
 void main() {
-  testWidgets('fullscreen media', (widgetTester) async {
-    await widgetTester.pumpWidget(
-      MaterialApp(
-        builder: (context, child) => StreamFeedTheme(
-          data: StreamFeedThemeData.light(),
-          child: child!,
+  group('fullscreen media', () {
+    testWidgets('find one GalleryHeader', (widgetTester) async {
+      await widgetTester.pumpWidget(
+        MaterialApp(
+          builder: (context, child) => StreamFeedTheme(
+            data: StreamFeedThemeData.light(),
+            child: child!,
+          ),
+          home: FullscreenMedia(
+            media: [
+              Media(
+                url:
+                    'https://i.picsum.photos/id/785/200/200.jpg?hmac=vvHnS4TgoGTRqwI2soaIhbOxE7Q-hhoZTTDe75h_fz4',
+              ),
+            ],
+          ),
         ),
-        home: FullscreenMedia(
-          media: [
-            Media(
-              url:
-                  'https://i.picsum.photos/id/785/200/200.jpg?hmac=vvHnS4TgoGTRqwI2soaIhbOxE7Q-hhoZTTDe75h_fz4',
-            ),
-          ],
-        ),
-      ),
-    );
+      );
 
-    expect(find.byType(GalleryHeader), findsOneWidget);
+      expect(find.byType(GalleryHeader), findsOneWidget);
+    });
+
+    testWidgets('find GalleryHeader text', (widgetTester) async {
+      await widgetTester.pumpWidget(
+        MaterialApp(
+          builder: (context, child) => StreamFeedTheme(
+            data: StreamFeedThemeData.light(),
+            child: child!,
+          ),
+          home: FullscreenMedia(
+            media: [
+              Media(
+                url:
+                'https://i.picsum.photos/id/785/200/200.jpg?hmac=vvHnS4TgoGTRqwI2soaIhbOxE7Q-hhoZTTDe75h_fz4',
+              ),
+            ],
+          ),
+        ),
+      );
+
+      expect(find.byType(Text), findsOneWidget);
+      expect(find.text('1 of 1'), findsOneWidget);
+    });
   });
 }
