39c01fe13c92c8f1364dc0c4b47cb03e8b8157bf
groovinchip <groovinchip@gmail.com>
2021-09-08 10:57:41 -0400

    test: add fullscreen_media_test.dart

---
+++ packages/stream_feed_flutter/test/fullscreen_media_test.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:stream_feed_flutter/stream_feed_flutter.dart';
+
+void main() {
+  testWidgets('fullscreen media', (widgetTester) async {
+    await widgetTester.pumpWidget(
+      MaterialApp(
+        builder: (context, child) => StreamFeedTheme(
+          data: StreamFeedThemeData.light(),
+          child: child!,
+        ),
+        home: FullscreenMedia(
+          media: [
+            Media(
+              url:
+                  'https://i.picsum.photos/id/785/200/200.jpg?hmac=vvHnS4TgoGTRqwI2soaIhbOxE7Q-hhoZTTDe75h_fz4',
+            ),
+          ],
+        ),
+      ),
+    );
+
+    expect(find.byType(FullscreenMedia), findsOneWidget);
+  });
+}
