314cd9f7d8b18824aa1bc7c7412467310d188cbe
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-09-09 11:32:53 -0400

    refactor mediaType test

--- packages/stream_feed_flutter/test/media/media_test.dart
+++ packages/stream_feed_flutter/test/media/media_test.dart
@@ -2,61 +2,25 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:stream_feed_flutter/stream_feed_flutter.dart';
 
 void main() {
-  group('mediaType works on all file formats', () {
-    test('jpeg', () {
-      final jpegMedia = Media(
-        url: 'https://someurl.com/images/test.jpeg',
-      );
-
-      expect(MediaType.image, jpegMedia.mediaType);
-    });
-
-    test('jpg', () {
-      final jpgMedia = Media(
-        url: 'https://someurl.com/images/test.jpg',
-      );
-
-      expect(MediaType.image, jpgMedia.mediaType);
-    });
-
-    test('png', () {
-      final pngMedia = Media(
-        url: 'https://someurl.com/images/test.png',
-      );
-
-      expect(MediaType.image, pngMedia.mediaType);
-    });
-
-    test('mp3', () {
-      final mp3Media = Media(
-        url: 'https://someurl.com/audio/test.mp3',
-      );
-
-      expect(MediaType.audio, mp3Media.mediaType);
-    });
-
-    test('wav', () {
-      final wavMedia = Media(
-        url: 'https://someurl.com/audio/test.wav',
-      );
-
-      expect(MediaType.audio, wavMedia.mediaType);
-    });
-
-    test('mp4', () {
-      final mp4Media = Media(
-        url: 'https://someurl.com/video/test.mp4',
-      );
-
-      expect(MediaType.video, mp4Media.mediaType);
-    });
-
-    test('unknown/unsupported', () {
-      final mp4Media = Media(
-        url: 'https://someurl.com/media/test.mov',
-      );
-
-      expect(MediaType.unknown, mp4Media.mediaType);
-    });
+  test('mediaType works on all file formats', () {
+    final urls = [
+      'https://someurl.com/images/test.jpeg',
+      'https://someurl.com/images/test.jpg',
+      'https://someurl.com/images/test.png',
+      'https://someurl.com/audio/test.mp3',
+      'https://someurl.com/audio/test.wav',
+      'https://someurl.com/video/test.mp4',
+      'https://someurl.com/media/test.mov'
+    ];
+    final mediaTypes = urls.map((url) => Media(url: url).mediaType).toList();
+    expect(mediaTypes, [
+      MediaType.image,
+      MediaType.image,
+      MediaType.image,
+      MediaType.audio,
+      MediaType.audio,
+      MediaType.video,
+      MediaType.unknown,
+    ]);
   });
 }
