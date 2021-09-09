f5a3f5c82547d98df832b1b5ea05c309b7543fcb
groovinchip <groovinchip@gmail.com>
2021-09-09 13:19:36 -0400

    docs: update Media.isValidUrl docs

--- packages/stream_feed_flutter/lib/src/media/media.dart
+++ packages/stream_feed_flutter/lib/src/media/media.dart
@@ -10,7 +10,7 @@ class Media {
   /// The URL for this media.
   final String url;
 
-  /// validate a string to check if it's a url or not
+  /// Validates a string to check if it's a url or not
   bool get isValidUrl => Uri.tryParse(url)?.hasAbsolutePath ?? false;
 
   /// Checks the [url] for specific file extensions and returns the
