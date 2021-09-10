71d703227ab54d2c02c5debb40423ba9c47a092f
groovinchip <groovinchip@gmail.com>
2021-09-10 10:55:40 -0400

    chore: sort exports

--- packages/faye_dart/lib/faye_dart.dart
+++ packages/faye_dart/lib/faye_dart.dart
@@ -1,6 +1,6 @@
 library faye_dart;
 
 export 'src/client.dart' show FayeClient, FayeClientState;
-export 'src/subscription.dart';
-export 'src/extensible.dart' show MessageHandler;
 export 'src/error.dart';
+export 'src/extensible.dart' show MessageHandler;
+export 'src/subscription.dart';
