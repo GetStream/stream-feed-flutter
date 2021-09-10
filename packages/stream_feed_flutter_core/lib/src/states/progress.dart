b00f9eb7c1c0144de969f70ecaa49d7e58242903
groovinchip <groovinchip@gmail.com>
2021-09-10 11:17:20 -0400

    chore: address analyzer warnings in progress.dart

--- packages/stream_feed_flutter_core/lib/src/states/progress.dart
+++ packages/stream_feed_flutter_core/lib/src/states/progress.dart
@@ -3,10 +3,13 @@ import 'package:flutter/material.dart';
 
 ///The default progress indicator to display progress state
 class ProgressStateWidget extends StatelessWidget {
+  /// Builds a [ProgressStateWidget].
   const ProgressStateWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: CircularProgressIndicator());
+    return const Center(
+      child: CircularProgressIndicator(),
+    );
   }
 }
