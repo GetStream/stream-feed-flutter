fd1518b7d3d2ab0f1ad9b8e1fcf27d847cf511ef
groovinchip <groovinchip@gmail.com>
2021-09-10 11:18:49 -0400

    chore: address analyzer warnings in empty.dart

--- packages/stream_feed_flutter_core/lib/src/states/empty.dart
+++ packages/stream_feed_flutter_core/lib/src/states/empty.dart
@@ -1,9 +1,13 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 ///An Empty State Widget to be used for empty states
 class EmptyStateWidget extends StatelessWidget {
-  const EmptyStateWidget({Key? key, this.message = 'Nothing here...'})
-      : super(key: key);
+  /// Builds an [EmptyStateWidget].
+  const EmptyStateWidget({
+    Key? key,
+    this.message = 'Nothing here...',
+  }) : super(key: key);
 
   ///The message to be displayed
   final String message;
@@ -11,5 +15,11 @@ class EmptyStateWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(child: Text(message));
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(StringProperty('message', message));
   }
 }
