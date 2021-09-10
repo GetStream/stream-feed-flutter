652c23165344442d87b41661eb24d8e32e106735
groovinchip <groovinchip@gmail.com>
2021-09-10 11:39:16 -0400

    chore: address analyzer warnings in flat_feed_test.dart

--- packages/stream_feed_flutter/test/flat_feed_test.dart
+++ packages/stream_feed_flutter/test/flat_feed_test.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:mocktail_image_network/mocktail_image_network.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:stream_feed_flutter/src/widgets/pages/flat_activity_list.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:mocktail_image_network/mocktail_image_network.dart';
+import 'package:stream_feed_flutter/src/widgets/pages/flat_activity_list.dart';
 import 'package:stream_feed_flutter_core/stream_feed_flutter_core.dart'
     hide FlatFeed;
+
 import 'mock.dart';
 
 void main() {
