5d9c7820b521b0bcc229477cab9bfcfac3f1cd3e
groovinchip <groovinchip@gmail.com>
2021-09-13 14:39:59 -0400

    test: add debug prop test for ActivityContent

--- packages/stream_feed_flutter/test/activity_test.dart
+++ packages/stream_feed_flutter/test/activity_test.dart
@@ -112,6 +112,7 @@ void main() {
         ),
       );
       final card = find.byType(ActivityCard);
+
       expect(card, findsOneWidget);
       final richtexts = tester.widgetList<Text>(find.byType(Text));
 
@@ -219,6 +220,40 @@ void main() {
 
     // ignore: cascade_invocations
     activityWidget.debugFillProperties(builder);
+
+    final description = builder.properties
+        .where((node) => !node.isFiltered(DiagnosticLevel.info))
+        .map((node) => node.toJsonMap(const DiagnosticsSerializationDelegate()))
+        .toList();
+
+    expect(description[0]['description'],
+        'EnrichedActivity<User, String, String, String>(User(null, {name: Rosemary, handle: @rosemary, subtitle: likes playing frisbee in the park, profile_image: https://randomuser.me/api/portraits/women/20.jpg}, null, null, null, null), null, null, null, null, null, null, ${now.toString()}, null, null, null, null, {image: https://handluggageonly.co.uk/wp-content/uploads/2017/08/IMG_0777.jpg}, null, null, null)');
+  });
+
+  testWidgets(
+      'Default ActivityContent debugFillProperties', (widgetTester) async {
+    final builder = DiagnosticPropertiesBuilder();
+    final now = DateTime.now();
+    final activityContent = ActivityContent(
+      activity: EnrichedActivity(
+        time: now,
+        actor: const User(
+          data: {
+            'name': 'Rosemary',
+            'handle': '@rosemary',
+            'subtitle': 'likes playing frisbee in the park',
+            'profile_image': 'https://randomuser.me/api/portraits/women/20.jpg',
+          },
+        ),
+        extraData: const {
+          'image':
+          'https://handluggageonly.co.uk/wp-content/uploads/2017/08/IMG_0777.jpg',
+        },
+      ),
+    );
+
+    // ignore: cascade_invocations
+    activityContent.debugFillProperties(builder);
 
     final description = builder.properties
         .where((node) => !node.isFiltered(DiagnosticLevel.info))
