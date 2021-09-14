0c6fa726df5d1e7886e656bee6d49a8bb8a324bb
groovinchip <groovinchip@gmail.com>
2021-09-14 10:00:19 -0400

    test: add debug prop test for ReactionToggleIcon

--- packages/stream_feed_flutter/test/button_test.dart
+++ packages/stream_feed_flutter/test/button_test.dart
@@ -12,6 +12,8 @@ import 'package:stream_feed_flutter/stream_feed_flutter.dart';
 
 import 'mock.dart';
 
+// ignore_for_file: cascade_invocations
+
 void main() {
   //TODO:fix me
   // testWidgets('ReactionList', (tester) async {
@@ -473,7 +475,6 @@ void main() {
         inactiveIcon: const Icon(Icons.favorite_border),
       );
 
-      // ignore: cascade_invocations
       childReactionButton.debugFillProperties(builder);
 
       final description = builder.properties
@@ -508,7 +509,6 @@ void main() {
         ),
       );
 
-      // ignore: cascade_invocations
       likeButton.debugFillProperties(builder);
 
       final description = builder.properties
@@ -545,8 +545,43 @@ void main() {
         inactiveIcon: const Icon(Icons.favorite_border),
       );
 
-      // ignore: cascade_invocations
       reactionButton.debugFillProperties(builder);
+
+      final description = builder.properties
+          .where((node) => !node.isFiltered(DiagnosticLevel.info))
+          .map((node) =>
+              node.toJsonMap(const DiagnosticsSerializationDelegate()))
+          .toList();
+
+      expect(description[0]['description'], 'null');
+    });
+
+    testWidgets('ReactionToggleIcon', (tester) async {
+      final builder = DiagnosticPropertiesBuilder();
+      final now = DateTime.now();
+      final reactionToggleIcon = ReactionToggleIcon(
+        activity: EnrichedActivity(
+          time: now,
+          actor: const User(
+            data: {
+              'name': 'Rosemary',
+              'handle': '@rosemary',
+              'subtitle': 'likes playing frisbee in the park',
+              'profile_image':
+                  'https://randomuser.me/api/portraits/women/20.jpg',
+            },
+          ),
+          extraData: const {
+            'image':
+                'https://handluggageonly.co.uk/wp-content/uploads/2017/08/IMG_0777.jpg',
+          },
+        ),
+        kind: 'comment',
+        activeIcon: const Icon(Icons.favorite),
+        inactiveIcon: const Icon(Icons.favorite_border),
+      );
+
+      reactionToggleIcon.debugFillProperties(builder);
 
       final description = builder.properties
           .where((node) => !node.isFiltered(DiagnosticLevel.info))
@@ -579,7 +614,6 @@ void main() {
         ),
       );
 
-      // ignore: cascade_invocations
       replyButton.debugFillProperties(builder);
 
       final description = builder.properties
@@ -603,23 +637,22 @@ void main() {
               'handle': '@rosemary',
               'subtitle': 'likes playing frisbee in the park',
               'profile_image':
-              'https://randomuser.me/api/portraits/women/20.jpg',
+                  'https://randomuser.me/api/portraits/women/20.jpg',
             },
           ),
           extraData: const {
             'image':
-            'https://handluggageonly.co.uk/wp-content/uploads/2017/08/IMG_0777.jpg',
+                'https://handluggageonly.co.uk/wp-content/uploads/2017/08/IMG_0777.jpg',
           },
         ),
       );
 
-      // ignore: cascade_invocations
       repostButton.debugFillProperties(builder);
 
       final description = builder.properties
           .where((node) => !node.isFiltered(DiagnosticLevel.info))
           .map((node) =>
-          node.toJsonMap(const DiagnosticsSerializationDelegate()))
+              node.toJsonMap(const DiagnosticsSerializationDelegate()))
           .toList();
 
       expect(description[0]['description'], 'null');
