6c1b91962513b9c6f60b7594ba56e0e84f050a1f
groovinchip <groovinchip@gmail.com>
2021-09-30 11:28:03 -0400

    test: add missing tests for gallery_preview_test.dart

--- packages/stream_feed_flutter/test/media/gallery_preview_test.dart
+++ packages/stream_feed_flutter/test/media/gallery_preview_test.dart
@@ -167,6 +167,56 @@ void main() {
       });
     });
 
+    testWidgets('Open and dismiss image', (tester) async {
+      await mockNetworkImages(() async {
+        await tester.pumpWidget(
+          MaterialApp(
+            builder: (context, child) => StreamFeedTheme(
+              data: StreamFeedThemeData.light(),
+              child: child!,
+            ),
+            home: Center(
+              child: GalleryPreview(
+                media: [
+                  Media(
+                    url:
+                        'https://i.picsum.photos/id/485/200/300.jpg?hmac=Kv8DZbgB5jppYcdfZdMVu2LM3XAIt-3fvR8VcmrLYhw',
+                  ),
+                  Media(
+                    url:
+                        'https://i.picsum.photos/id/11/200/300.jpg?hmac=n9AzdbWCOaV1wXkmrRfw5OulrzXJc0PgSFj4st8d6ys',
+                  ),
+                  Media(
+                    url:
+                        'https://i.picsum.photos/id/373/200/300.jpg?hmac=GXSHLvl-WsHouC5yVXzXVLNnpn21lCdp5rjUE_wyK-8',
+                  ),
+                  Media(
+                    url:
+                        'https://i.picsum.photos/id/584/200/300.jpg?hmac=sBfls3kxMp0j0qH3R-K2qM8Wyak1FlpOIgtcd7cEg68',
+                  ),
+                  Media(
+                    url:
+                        'https://i.picsum.photos/id/779/200/300.jpg?hmac=DmFN06G0c1N5TAbj2O9YljZ0Vr8VWOZ4lPjLG8oAf8o',
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+
+        final plusOneButton = find.text('+ 1');
+        expect(plusOneButton, findsOneWidget);
+
+        await tester.tap(plusOneButton);
+        await tester.pumpAndSettle();
+
+        final backButton = find.byType(StreamSvgIcon);
+        expect(backButton, findsOneWidget);
+
+        expect(() => tester.tap(backButton), returnsNormally);
+      });
+    });
+
     testWidgets('debugFillProperties', (tester) async {
       final builder = DiagnosticPropertiesBuilder();
       const GalleryPreview(media: []).debugFillProperties(builder);
@@ -227,6 +277,58 @@ void main() {
 
         expect(find.byType(GestureDetector), findsOneWidget);
         expect(find.byType(Image), findsOneWidget);
+      });
+    });
+
+    testWidgets('tap on image', (tester) async {
+      await mockNetworkImages(() async {
+        await tester.pumpWidget(
+          MaterialApp(
+            builder: (context, child) => StreamFeedTheme(
+              data: StreamFeedThemeData.light(),
+              child: child!,
+            ),
+            home: Center(
+              child: Flex(
+                direction: Axis.vertical,
+                children: [
+                  Flexible(
+                    fit: FlexFit.tight,
+                    child: Flex(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      direction: Axis.horizontal,
+                      children: [
+                        FlexibleImage(
+                          media: [
+                            Media(
+                              url:
+                                  'https://i.picsum.photos/id/485/200/300.jpg?hmac=Kv8DZbgB5jppYcdfZdMVu2LM3XAIt-3fvR8VcmrLYhw',
+                            ),
+                          ],
+                          child: Image.network(
+                            'https://i.picsum.photos/id/485/200/300.jpg?hmac=Kv8DZbgB5jppYcdfZdMVu2LM3XAIt-3fvR8VcmrLYhw',
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+
+        final gestureDetector = find.byType(GestureDetector);
+        expect(gestureDetector, findsOneWidget);
+
+        await tester.tap(gestureDetector);
+        await tester.pumpAndSettle();
+
+        final backButton = find.byType(StreamSvgIcon);
+        expect(backButton, findsOneWidget);
+
+        expect(() => tester.tap(backButton), returnsNormally); 
       });
     });
 
