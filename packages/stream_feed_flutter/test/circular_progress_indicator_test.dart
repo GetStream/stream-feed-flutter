7d717c29cb730769ad0b4594924df3ab5ef0e7ac
groovinchip <groovinchip@gmail.com>
2021-10-01 12:04:20 -0400

    test: add circular_progress_indicator_test.dart

---
+++ packages/stream_feed_flutter/test/circular_progress_indicator_test.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:stream_feed_flutter/src/widgets/circular_progress_indicator.dart';
+
+void main() {
+  group('CircularProgressIndicator tests', () {
+    testWidgets('When no loading progress is given, show the child',
+        (tester) async {
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: Center(
+            child: StreamCircularProgressIndicator(
+              child: SizedBox.shrink(),
+            ),
+          ),
+        ),
+      );
+
+      expect(find.byType(SizedBox), findsOneWidget);
+    });
+
+    testWidgets('debugFillProperties', (tester) async {
+      final builder = DiagnosticPropertiesBuilder();
+      const StreamCircularProgressIndicator(
+        child: SizedBox.shrink(),
+      ).debugFillProperties(builder);
+
+      final description = builder.properties
+          .where((node) => !node.isFiltered(DiagnosticLevel.info))
+          .map((node) =>
+              node.toJsonMap(const DiagnosticsSerializationDelegate()))
+          .toList();
+
+      expect(description[0]['description'], 'null');
+    });
+  });
+}
