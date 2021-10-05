6cc0694979962d409f2b13aa2abde3fb07128d0a
groovinchip <groovinchip@gmail.com>
2021-10-05 15:43:10 -0400

    test: add interactive_text_test.dart

---
+++ packages/stream_feed_flutter/test/interactive_text_test.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:stream_feed_flutter/src/utils/tag_detector.dart';
+import 'package:stream_feed_flutter/src/widgets/interactive_text.dart';
+
+void main() {
+  testWidgets('debugFillProperties', (tester) async {
+    final builder = DiagnosticPropertiesBuilder();
+    const InteractiveText(
+      tagged: TaggedText(
+        tag: Tag.normalText,
+        text: 'Test',
+      ),
+    ).debugFillProperties(builder);
+
+    final description = builder.properties
+        .where((node) => !node.isFiltered(DiagnosticLevel.info))
+        .map((node) => node.toJsonMap(const DiagnosticsSerializationDelegate()))
+        .toList();
+
+    expect(description[0]['description'], 'TaggedText(Tag.normalText, Test)');
+  });
+}
