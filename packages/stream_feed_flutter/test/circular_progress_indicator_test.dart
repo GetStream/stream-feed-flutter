afb7a6b07baabb5fae9a5ac9b7dca520aa34392f
groovinchip <groovinchip@gmail.com>
2021-10-05 15:36:36 -0400

    chore: remove unused code

--- packages/stream_feed_flutter/test/circular_progress_indicator_test.dart
+++ packages/stream_feed_flutter/test/circular_progress_indicator_test.dart
@@ -2,14 +2,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:stream_feed_flutter/src/widgets/circular_progress_indicator.dart';
-import 'package:mocktail/mocktail.dart';
-
-class MockImageChunkEvent extends Mock implements ImageChunkEvent {
-  @override
-  String toString({DiagnosticLevel minLevel = DiagnosticLevel.info}) {
-    return super.toString();
-  }
-}
 
 void main() {
   group('CircularProgressIndicator tests', () {
