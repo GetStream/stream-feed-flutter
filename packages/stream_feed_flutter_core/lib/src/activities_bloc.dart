ae5e10aa36d86db07aa13e7b9b2736cfc5df7fbc
groovinchip <groovinchip@gmail.com>
2021-10-18 14:15:59 -0400

    fix(core): fixed a bug that caused duplicate activity entries when testing

--- packages/stream_feed_flutter_core/lib/src/activities_bloc.dart
+++ packages/stream_feed_flutter_core/lib/src/activities_bloc.dart
@@ -75,7 +75,7 @@ class FeedBloc<A, Ob, T, Or> {
     final enrichedActivity = await flatFeed
         .getEnrichedActivityDetail<A, Ob, T, Or>(addedActivity.id!);
 
-    final _activities = activities ?? [enrichedActivity];
+    final _activities = activities ?? [];
 
     // ignore: cascade_invocations
     _activities.insert(0, enrichedActivity);
