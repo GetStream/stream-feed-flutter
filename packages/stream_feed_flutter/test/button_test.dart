dbbf66ea3df916658163d7a4375cdd8e2f569f2b
GroovinChip <groovinchip@gmail.com>
2021-10-26 13:53:35 -0400

    test(flutter): fix failing tests in button_test.dart

--- packages/stream_feed_flutter/test/button_test.dart
+++ packages/stream_feed_flutter/test/button_test.dart
@@ -49,12 +49,17 @@ void main() {
           kind: kind,
         )).thenAnswer((_) async => reactions);
     await tester.pumpWidget(
-      StreamFeed(
-        bloc: GenericFeedBloc(
-          client: mockClient,
-          analyticsClient: mockStreamAnalytics,
-        ),
-        child: Scaffold(
+      MaterialApp(
+        builder: (context, child) {
+          return StreamFeed(
+            bloc: GenericFeedBloc(
+              client: mockClient,
+              analyticsClient: mockStreamAnalytics,
+            ),
+            child: child!,
+          );
+        },
+        home: Scaffold(
           body: ReactionListView(
             activity: const GenericEnrichedActivity(id: 'id'),
             reactionBuilder: (context, reaction) => const Offstage(),
@@ -168,7 +173,7 @@ void main() {
             GenericEnrichedActivity<User, String, String, String>(
           id: 'id',
           time: now,
-          actor: User(data: const {
+          actor: const User(data: {
             'name': 'Rosemary',
             'handle': '@rosemary',
             'subtitle': 'likes playing fresbee in the park',
@@ -189,9 +194,15 @@ void main() {
         when(() => mockClient.reactions).thenReturn(mockReactions);
         when(() => mockReactions.addChild('like', parentId))
             .thenAnswer((_) async => childReaction);
-        await tester.pumpWidget(StreamFeed(
-            bloc: bloc,
-            child: Scaffold(
+        await tester.pumpWidget(
+          MaterialApp(
+            builder: (context, child) {
+              return StreamFeed(
+                bloc: bloc,
+                child: child!,
+              );
+            },
+            home: Scaffold(
               body: ChildReactionToggleIcon(
                 activity: reactedActivity,
                 hoverColor: Colors.lightBlue,
@@ -201,7 +212,9 @@ void main() {
                 inactiveIcon: inactiveIcon,
                 activeIcon: activeIcon,
               ),
-            )));
+            ),
+          ),
+        );
         final reactionIcon = find.byType(ReactionIcon);
         expect(reactionIcon, findsOneWidget);
         await tester.tap(reactionIcon);
@@ -344,8 +357,8 @@ void main() {
           childrenCounts: const {
             'like': 0,
           },
-          latestChildren: const {'like': const []},
-          ownChildren: const {'like': const []},
+          latestChildren: const {'like': []},
+          ownChildren: const {'like': []},
         )
       ];
       when(() => mockClient.reactions).thenReturn(mockReactions);
@@ -400,25 +413,34 @@ void main() {
           .thenAnswer((invocation) => Future.value());
 
       await tester.pumpWidgetBuilder(
-        StreamFeed(
-          bloc: bloc,
-          child: Scaffold(
-              body: ReactionToggleIcon(
-            ownReactions: const [reaction],
-            activity:
-                GenericEnrichedActivity(id: activityId, reactionCounts: const {
-              'like': 1300
-            }, ownReactions: const {
-              'like': [reaction]
-            }, latestReactions: const {
-              'like': [reaction]
-            }),
-            feedGroup: feedGroup,
-            kind: kind,
-            activeIcon: activeIcon,
-            count: 1300,
-            inactiveIcon: inactiveIcon,
-          )),
+        MaterialApp(
+          builder: (context, child) {
+            return StreamFeed(
+              bloc: bloc,
+              child: child!,
+            );
+          },
+          home: Scaffold(
+            body: ReactionToggleIcon(
+              ownReactions: const [reaction],
+              activity: GenericEnrichedActivity(
+                  id: activityId,
+                  reactionCounts: const {
+                    'like': 1300
+                  },
+                  ownReactions: const {
+                    'like': [reaction]
+                  },
+                  latestReactions: const {
+                    'like': [reaction]
+                  }),
+              feedGroup: feedGroup,
+              kind: kind,
+              activeIcon: activeIcon,
+              count: 1300,
+              inactiveIcon: inactiveIcon,
+            ),
+          ),
         ),
         surfaceSize: const Size(125, 100),
       );
