406648f6ab8f5fb76b997e2d160933ae51b4fb14
GroovinChip <groovinchip@gmail.com>
2021-10-26 15:34:24 -0400

    test(core): fix failing tests in reactions_bloc_test.dart

--- packages/stream_feed_flutter_core/test/bloc/reactions_bloc_test.dart
+++ packages/stream_feed_flutter_core/test/bloc/reactions_bloc_test.dart
@@ -92,6 +92,7 @@ void main() {
     mockSecondUser = MockStreamUser();
     secondUserId = '2';
     when(() => mockClient.flatFeed('user', 'test')).thenReturn(mockFeed);
+    when(() => mockClient.flatFeed('timeline')).thenReturn(mockFeed);
     when(() => mockClient.flatFeed('user', userId)).thenReturn(mockFeed);
     when(() => mockClient.flatFeed('user', secondUserId)).thenReturn(mockFeed);
     when(() => mockFeed.addActivity(activity))
@@ -139,8 +140,14 @@ void main() {
     });
 
     test('onAddReaction', () async {
-      final addedReaction = Reaction();
+      final controller = ActivitiesControllers();
+      const addedReaction = Reaction(id: '1');
+      // ignore: cascade_invocations
+      controller.init(feedGroup);
+      bloc.activitiesController = controller;
+      // ignore: cascade_invocations
       bloc.reactionsControllers = mockReactionControllers;
+      expect(bloc.activitiesController.hasValue(feedGroup), true);
       when(() => mockReactionControllers.getReactions(activityId))
           .thenAnswer((_) => reactions);
       expect(bloc.reactionsControllers.getReactions(activityId), reactions);
@@ -150,9 +157,10 @@ void main() {
           )).thenAnswer((_) async => addedReaction);
 
       await bloc.onAddReaction(
-          activity: GenericEnrichedActivity(id: activityId),
-          feedGroup: feedGroup,
-          kind: kind);
+        activity: GenericEnrichedActivity(id: activityId),
+        feedGroup: feedGroup,
+        kind: kind,
+      );
       verify(() => mockReactions.add(
             kind,
             activityId,
@@ -162,9 +170,9 @@ void main() {
           emits([
             GenericEnrichedActivity(id: activityId, reactionCounts: const {
               'like': 1
-            }, ownReactions: {
+            }, ownReactions: const {
               'like': [addedReaction]
-            }, latestReactions: {
+            }, latestReactions: const {
               'like': [addedReaction]
             })
           ]));
@@ -172,8 +180,12 @@ void main() {
       //TODO: test reaction Stream
     });
     test('onRemoveReaction', () async {
+      final controller = ActivitiesControllers();
       const reactionId = 'reactionId';
-      final reaction = Reaction(id: reactionId);
+      const reaction = Reaction(id: reactionId);
+      controller.init(feedGroup);
+      bloc.activitiesController = controller;
+      // ignore: cascade_invocations
       bloc.reactionsControllers = mockReactionControllers;
       when(() => mockReactionControllers.getReactions(activityId))
           .thenAnswer((_) => reactions);
@@ -185,9 +197,9 @@ void main() {
         activity:
             GenericEnrichedActivity(id: activityId, reactionCounts: const {
           'like': 1
-        }, ownReactions: {
+        }, ownReactions: const {
           'like': [reaction]
-        }, latestReactions: {
+        }, latestReactions: const {
           'like': [reaction]
         }),
         feedGroup: feedGroup,
@@ -217,12 +229,15 @@ void main() {
         final reactedActivity = GenericEnrichedActivity(
           id: 'id',
           time: now,
-          actor: User(data: const {
-            'name': 'Rosemary',
-            'handle': '@rosemary',
-            'subtitle': 'likes playing fresbee in the park',
-            'profile_image': 'https://randomuser.me/api/portraits/women/20.jpg',
-          }),
+          actor: const User(
+            data: {
+              'name': 'Rosemary',
+              'handle': '@rosemary',
+              'subtitle': 'likes playing fresbee in the park',
+              'profile_image':
+                  'https://randomuser.me/api/portraits/women/20.jpg',
+            },
+          ),
         );
         controller.init(reactedActivity.id!);
         bloc.reactionsControllers = controller;
@@ -323,6 +338,10 @@ void main() {
     test(
         '''When we await onAddActivity the stream gets updated with the new expected value''',
         () async {
+      final controller = ActivitiesControllers();
+      // ignore: cascade_invocations
+      controller.init(feedGroup);
+      bloc.activitiesController = controller;
       await bloc.onAddActivity(
         feedGroup: 'user',
         verb: 'post',
@@ -333,29 +352,42 @@ void main() {
       verify(() => mockFeed.getEnrichedActivityDetail(addedActivity.id!))
           .called(1);
       await expectLater(
-          bloc.getActivities(feedGroup), emits([enrichedActivity]));
-    });
-  });
-
-  group('currentUser', () {
-    test('currentUser matches', () {
-      expect(bloc.currentUser, mockUser);
+          bloc.getActivitiesStream(feedGroup), emits([enrichedActivity]));
     });
   });
 
   group('Follows', () {
     test('isFollowingUser', () async {
+      when(() => mockClient.flatFeed('timeline')).thenReturn(mockFeed);
+      when(() => mockFeed.following(
+            limit: 1,
+            offset: 0,
+            filter: [
+              FeedId.id('user:2'),
+            ],
+          )).thenAnswer((_) async => following);
+
       final isFollowing = await bloc.isFollowingUser('2');
       expect(isFollowing, false);
     });
 
-    test('followUser', () async {
-      await bloc.followFlatFeed(mockSecondUser.id);
+    test('followFlatFeed', () async {
+      when(() => mockClient.flatFeed('timeline')).thenReturn(mockFeed);
+      when(() => mockClient.flatFeed('user', '2')).thenReturn(mockSecondFeed);
+      when(() => mockFeed.follow(mockSecondFeed))
+          .thenAnswer((_) => Future.value());
+
+      await bloc.followFlatFeed('2');
       verify(() => mockFeed.follow(mockSecondFeed)).called(1);
     });
 
-    test('unfollowUser', () async {
-      await bloc.unfollowFlatFeed(mockSecondUser.id);
+    test('unfollowFlatFeed', () async {
+      when(() => mockClient.flatFeed('timeline')).thenReturn(mockFeed);
+      when(() => mockClient.flatFeed('user', '2')).thenReturn(mockSecondFeed);
+      when(() => mockFeed.unfollow(mockSecondFeed))
+          .thenAnswer((_) => Future.value());
+
+      await bloc.unfollowFlatFeed('2');
       verify(() => mockFeed.unfollow(mockSecondFeed)).called(1);
     });
   });
