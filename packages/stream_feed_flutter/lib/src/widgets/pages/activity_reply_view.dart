cdf1ce0494beb33499ada4acd1d0faad027b2570
GroovinChip <groovinchip@gmail.com>
2021-11-05 12:36:53 -0400

    docs(flutter): add docs to ActivityReplyView

--- packages/stream_feed_flutter/lib/src/widgets/pages/activity_reply_view.dart
+++ packages/stream_feed_flutter/lib/src/widgets/pages/activity_reply_view.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:stream_feed_flutter/src/widgets/activity/activity.dart';
 import 'package:stream_feed_flutter/stream_feed_flutter.dart';
 
+/// A widget that provides the means to reply to an activity.
+///
+/// It displays the parent activity with a reply area underneath. It also
+/// provides a button to actually post the activity.
 class ActivityReplyView extends StatefulWidget {
   const ActivityReplyView({
     Key? key,
@@ -9,7 +13,10 @@ class ActivityReplyView extends StatefulWidget {
     required this.feedGroup,
   }) : super(key: key);
 
+  /// The activity to reply to.
   final EnrichedActivity parentActivity;
+
+  /// The feed group that the [parentActivity] belongs to.
   final String feedGroup;
 
   @override
