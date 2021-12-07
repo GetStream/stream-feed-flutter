0e672d939f177b6e0d12df5f9b2a333e6a25f1e3
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-12-07 10:17:31 -0400

    update sample app

--- packages/stream_feed_flutter_core/example/lib/main.dart
+++ packages/stream_feed_flutter_core/example/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:stream_feed_flutter_core/stream_feed_flutter_core.dart';
 
@@ -24,41 +25,74 @@ Future<void> main() async {
   );
 
   runApp(
-    MaterialApp(
+    MyApp(client: client),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    Key? key,
+    required this.client,
+  }) : super(key: key);
+
+  final StreamFeedClient client;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       builder: (context, child) => FeedProvider(
         bloc: FeedBloc(
           client: client,
         ),
         child: child!,
       ),
-      home: Scaffold(
-        body: FlatFeedCore(
-          feedGroup: 'user',
-          userId: client.currentUser!.id,
-          feedBuilder: (BuildContext context, activities, int index) {
-            return InkWell(
-              child: ListTile(
-                title: Text('${activities[index].actor!.data!['handle']}'),
-                subtitle: Text('${activities[index].object}'),
-              ),
-              onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute<void>(
-                    builder: (BuildContext context) => Scaffold(
-                      body: ReactionListCore(
-                        lookupValue: activities[index].id!,
-                        reactionsBuilder: (context, reactions, idx) => Text(
-                          '${reactions[index].data?['text']}',
-                        ),
+      home: HomePage(client: client),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({
+    Key? key,
+    required this.client,
+  }) : super(key: key);
+
+  final StreamFeedClient client;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FlatFeedCore(
+        feedGroup: 'user',
+        userId: widget.client.currentUser!.id,
+        feedBuilder: (BuildContext context, activities, int index) {
+          return InkWell(
+            child: ListTile(
+              title: Text('${activities[index].actor!.data!['handle']}'),
+              subtitle: Text('${activities[index].object}'),
+            ),
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute<void>(
+                  builder: (BuildContext context) => Scaffold(
+                    body: ReactionListCore(
+                      lookupValue: activities[index].id!,
+                      reactionsBuilder: (context, reactions, idx) => Text(
+                        '${reactions[index].data?['text']}',
                       ),
                     ),
                   ),
-                );
-              },
-            );
-          },
-        ),
+                ),
+              );
+            },
+          );
+        },
       ),
-    ),
-  );
+    );
+  }
 }
