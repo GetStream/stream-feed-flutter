051fa871de3252099e2a9735e8f222d58771a31f
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-12-20 15:55:50 +0100

    fix FeedProvider test

--- packages/stream_feed_flutter_core/test/stream_feed_flutter_core_test.dart
+++ packages/stream_feed_flutter_core/test/stream_feed_flutter_core_test.dart
@@ -95,7 +95,7 @@ void main() {
           () => FeedProvider.of(childKey.currentState!.context),
           throwsA(predicate<AssertionError>((e) =>
               e.message ==
-              'No GenericFeedProvider<User, String, String, String> found in context')));
+              'No `FeedProvider` or `GenericFeedProvider<User, String, String, String>` found in context')));
     },
   );
 }
