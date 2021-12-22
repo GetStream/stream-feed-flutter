81e6c77c47625dbc697bedb666eb969542b13707
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-12-22 13:24:08 +0100

    llc: lint sort exports

--- packages/stream_feed/lib/src/client/index.dart
+++ packages/stream_feed/lib/src/client/index.dart
@@ -1,7 +1,7 @@
 export 'analytics_client.dart';
-export 'image_storage_client.dart';
 export 'file_storage_client.dart';
 export 'flat_feed.dart';
+export 'image_storage_client.dart';
 export 'reactions_client.dart';
 export 'stream_feed_client.dart';
 export 'stream_user.dart';
