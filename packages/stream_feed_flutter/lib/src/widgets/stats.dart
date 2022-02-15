9f72df4ff19a6ef4c4a3a516ae9b7091a2f0d040
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2022-02-15 16:48:27 -0400

    ui kit: FollowStatsWidget InkWell

--- packages/stream_feed_flutter/lib/src/widgets/stats.dart
+++ packages/stream_feed_flutter/lib/src/widgets/stats.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:stream_feed_flutter/stream_feed_flutter.dart';
 import 'package:stream_feed_flutter_core/stream_feed_flutter_core.dart';
 
 class FollowStatsWidget extends StatelessWidget {
@@ -10,31 +11,49 @@ class FollowStatsWidget extends StatelessWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
-        Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              '${user?.followersCount ?? bloc.currentUser!.followersCount}',
-              style: Theme.of(context).textTheme.headline6,
-            ),
-            Text(
-              'Followers',
-              style: Theme.of(context).textTheme.bodyText1,
-            ),
-          ],
+        InkWell(
+          onTap: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (_) => const FollowersScreen(),
+              ),
+            );
+          },
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                '${user?.followersCount ?? bloc.currentUser!.followersCount}',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              Text(
+                'Followers',
+                style: Theme.of(context).textTheme.bodyText1,
+              ),
+            ],
+          ),
         ),
-        Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              '${user?.followingCount ?? bloc.currentUser!.followingCount}',
-              style: Theme.of(context).textTheme.headline6,
-            ),
-            Text(
-              'Following',
-              style: Theme.of(context).textTheme.bodyText1,
-            ),
-          ],
+        InkWell(
+          onTap: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (_) => const FollowingScreen(),
+              ),
+            );
+          },
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                '${user?.followingCount ?? bloc.currentUser!.followingCount}',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              Text(
+                'Following',
+                style: Theme.of(context).textTheme.bodyText1,
+              ),
+            ],
+          ),
         ),
       ],
     );
