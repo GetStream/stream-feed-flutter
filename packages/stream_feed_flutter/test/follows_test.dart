3f31c0216791c485b90a16233358193f6600174c
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2022-02-15 11:10:18 -0400

    ui kit: FollowButton test

--- packages/stream_feed_flutter/test/follows_test.dart
+++ packages/stream_feed_flutter/test/follows_test.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:stream_feed_flutter/stream_feed_flutter.dart';
-
+import 'package:mocktail/mocktail.dart';
 import 'mock.dart';
 
 void main() {
+  late MockStreamFeedClient mockClient;
+  setUp(() {
+    mockClient = MockStreamFeedClient();
+  });
+
   // test('FollowingListView', () {
 
   // });
@@ -14,7 +19,7 @@ void main() {
   // });
 
   testWidgets('FollowStatsWidget', (tester) async {
-    final mockClient = MockStreamFeedClient();
+    mockClient = MockStreamFeedClient();
     await tester.pumpWidget(MaterialApp(
         builder: (context, child) {
           return StreamFeed(
@@ -31,8 +36,38 @@ void main() {
     final followingCount = find.text('3');
     expect(followersCount, findsOneWidget);
     expect(followingCount, findsOneWidget);
-  });
-  // test('FollowButton', () {
+    final followers = find.text('Followers');
+    final following = find.text('Following');
 
-  // });
+    expect(followers, findsOneWidget);
+    expect(following, findsOneWidget);
+  });
+  testWidgets('FollowButton', (tester) async {
+    final mockFeed = MockFlatFeed();
+    when(() => mockClient.flatFeed('timeline')).thenReturn(mockFeed);
+    when(() => mockFeed.following(
+          limit: 1,
+          offset: 0,
+          filter: [
+            FeedId.id('user:2'),
+          ],
+        )).thenAnswer((_) async => <Follow>[]);
+
+    // final isFollowing = await bloc.isFollowingFeed(followerId: '2');
+    await tester.pumpWidget(MaterialApp(
+        builder: (context, child) {
+          return StreamFeed(
+            bloc: FeedBloc(client: mockClient),
+            child: child!,
+          );
+        },
+        home: const Scaffold(
+            body: FollowButton(
+          user: User(id: '2', followersCount: 1, followingCount: 3),
+        ))));
+ 
+    await tester.pumpAndSettle();
+    expect(find.byType(OutlinedButton), findsOneWidget);
+       expect(find.text('Follow'), findsOneWidget);
+  });
 }
