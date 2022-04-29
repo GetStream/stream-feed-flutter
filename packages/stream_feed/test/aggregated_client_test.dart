63b7c91ea56491a6c4d5055e1862151c9061087d
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2022-04-29 10:41:16 -0400

    test getEnrichedActivity

--- packages/stream_feed/test/aggregated_client_test.dart
+++ packages/stream_feed/test/aggregated_client_test.dart
@@ -22,6 +22,44 @@ void main() {
     const token = Token('dummyToken');
     final client = AggregatedFeed(feedId, api, userToken: token);
 
+    test('getEnrichedActivityDetail', () async {
+      const limit = 1;
+      const activityId = 'e561de8f-00f1-11e4-b400-0cc47a024be0';
+      final filter = Filter()
+          .idLessThanOrEqual(activityId)
+          .idGreaterThanOrEqual(activityId);
+      final options = {
+        'limit': limit,
+        'offset': Default.offset, //TODO:add session everywhere
+        ...filter.params,
+        ...Default.marker.params,
+      };
+
+      final rawActivities = [jsonFixture('group.json')];
+      when(() => api.getEnrichedActivities(token, feedId, options))
+          .thenAnswer((_) async => Response(
+              data: {'results': rawActivities},
+              requestOptions: RequestOptions(
+                path: '',
+              ),
+              statusCode: 200));
+      final activities = await client.getEnrichedActivityDetail<String, String,
+          String, String>(activityId);
+
+      expect(
+          activities,
+          rawActivities
+              .map((e) => Group<
+                      GenericEnrichedActivity<String, String, String,
+                          String>>.fromJson(
+                  e,
+                  (o) => GenericEnrichedActivity<String, String, String,
+                      String>.fromJson(o as Map<String, dynamic>)))
+              .toList(growable: false)
+              .first);
+      verify(() => api.getEnrichedActivities(token, feedId, options)).called(1);
+    });
+
     test('getActivityDetail', () async {
       const limit = 1;
       const activityId = 'e561de8f-00f1-11e4-b400-0cc47a024be0';
